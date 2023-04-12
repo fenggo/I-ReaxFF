9b34d2b11f5e1037cfd1bd419a00a6a80f7aa176
FengGo <gfeng.alan@foxmail.com>
2023-04-12 14:32:22 +0000

    update setup.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install with commond
 '''
 
 
-__version__ = '1.4.1'
+__version__ = '1.5.2'
 install_requires = ['numpy','ase','tensorflow','matplotlib','paramiko']
 url = "https://gitee.com/fenggo/I-ReaxFF"
 
