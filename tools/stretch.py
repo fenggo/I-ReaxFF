f7394940a6f18847b7627824b36ac4bf0ed02533
FengGo <gfeng.alan@foxmail.com>
2023-04-17 00:24:17 +0000

    update tools/stretch.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/stretch.py
+++ tools/stretch.py
@@ -16,6 +16,7 @@ parser.add_argument('--i',default=0,type=int, help='iatom')
 parser.add_argument('--j',default=1,type=int, help='jatom')
 parser.add_argument('--s',default=1.4,type=float, help='start radius')
 parser.add_argument('--e',default=1.9,type=float, help='end radius')
+parser.add_argument('--auto',default=1,type=int, help='automaticly determin the moving group')
 parser.add_argument('--f',default=-1,type=int, help='the trajectory frame')
 
 args    = parser.parse_args(sys.argv[1:])
@@ -23,7 +24,15 @@ args    = parser.parse_args(sys.argv[1:])
 
 atoms   = read(args.g,index=args.f)
 ad      = AtomDance(atoms=atoms)
-images  = ad.stretch([args.i,args.j],nbin=50,rst=args.s,red=args.e,scale=1.26,traj='md.traj')
+
+if args.auto:
+   to_move = None
+else:
+   to_move = [args.j]
+
+images  = ad.stretch([args.i,args.j],nbin=50,rst=args.s,red=args.e,
+                     ToBeMoved=to_move,
+                     scale=1.26,traj='md.traj')
 ad.close()
 
 
