9599f7335fd5c162f04116b87fe9cb88019e4f40
FengGo <gfeng.alan@foxmail.com>
2023-07-09 10:08:19 +0000

    update irff/deb/compare_energies.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/deb/compare_energies.py
+++ irff/deb/compare_energies.py
@@ -176,7 +176,7 @@ def deb_gulp_energy(images,atomi=0,atomj=1,ffield='reax.lib',show=False):
            GE['etorsion'],GE['fconj'],GE['evdw'],GE['ehb'],GE['ecoul']
 
 if __name__ == '__main__':
-    images = Trajectory('hmx.traj')
+    images = Trajectory('md.traj')
     irff_energy = deb_energy(images=images)
     # ffieldtolib()
     gulp_energy = deb_gulp_energy(images, ffield='reaxff_nn')
