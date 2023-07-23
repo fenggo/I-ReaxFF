49e9a8b422effef8e9829e845dfa30f9ba06a48d
FengGo <gfeng.alan@foxmail.com>
2023-07-23 12:05:03 +0000

    lammps script
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/lmd.py
+++ tools/lmd.py
@@ -9,13 +9,16 @@ from irff.md.lammps import writeLammpsData,writeLammpsIn,get_lammps_thermal
 def nvt(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
         x=1,y=1,z=1,n=1,lib='ffield'):
     atoms = read(gen,index=i)*(x,y,z)
+    symbols = atoms.get_chemical_symbols()
+    species = sorted(set(symbols))
+    sp      = ''.join(species)
     writeLammpsData(atoms,data='data.lammps',specorder=None, 
                     masses={'Al':26.9820,'C':12.0000,'H':1.0080,'O':15.9990,
                              'N':14.0000,'F':18.9980},
                     force_skew=False,
                     velocities=False,units="real",atom_style='charge')
     writeLammpsIn(log='lmp.log',timestep=timestep,total=step,restart=None,
-              pair_coeff ='* * ffield C H O N',
+              pair_coeff ='* * {:s} {:s}'.format(lib,sp),
               pair_style = 'reaxff control checkqeq yes',  # without lg set lgvdw no
               fix = 'fix   1 all nvt temp 300 300 100.0 ',
               fix_modify = ' ',
