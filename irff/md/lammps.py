74fd8e3dd2d040c5b8d3fa53eebdb171d9ff4989
FengGo <gfeng.alan@foxmail.com>
2023-08-07 15:01:50 +0000

    lammps script
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/md/lammps.py
+++ irff/md/lammps.py
@@ -463,7 +463,91 @@ def LammpsHistory(traj='lammps.trj',atomType=['C','H','O','N']):
     lines= None
     return atoms
 
+def read_lammps_dump_text(fileobj, index=-1, **kwargs):
+    """Process cleartext lammps dumpfiles
 
+    :param fileobj: filestream providing the trajectory data
+    :param index: integer or slice object (default: get the last timestep)
+    :returns: list of Atoms objects
+    :rtype: list
+    """
+    # Load all dumped timesteps into memory simultaneously
+    lines = deque(fileobj.readlines())
+    index_end = get_max_index(index)
+
+    n_atoms = 0
+    images = []
+
+    # avoid references before assignment in case of incorrect file structure
+    cell, celldisp, pbc = None, None, False
+
+    while len(lines) > n_atoms:
+        line = lines.popleft()
+
+        if "ITEM: TIMESTEP" in line:
+            n_atoms = 0
+            line = lines.popleft()
+            # !TODO: pyflakes complains about this line -> do something
+            # ntimestep = int(line.split()[0])  # NOQA
+
+        if "ITEM: NUMBER OF ATOMS" in line:
+            line = lines.popleft()
+            n_atoms = int(line.split()[0])
+
+        if "ITEM: BOX BOUNDS" in line:
+            # save labels behind "ITEM: BOX BOUNDS" in triclinic case
+            # (>=lammps-7Jul09)
+            tilt_items = line.split()[3:]
+            celldatarows = [lines.popleft() for _ in range(3)]
+            celldata = np.loadtxt(celldatarows)
+            diagdisp = celldata[:, :2].reshape(6, 1).flatten()
+
+            # determine cell tilt (triclinic case!)
+            if len(celldata[0]) > 2:
+                # for >=lammps-7Jul09 use labels behind "ITEM: BOX BOUNDS"
+                # to assign tilt (vector) elements ...
+                offdiag = celldata[:, 2]
+                # ... otherwise assume default order in 3rd column
+                # (if the latter was present)
+                if len(tilt_items) >= 3:
+                    sort_index = [tilt_items.index(i)
+                                  for i in ["xy", "xz", "yz"]]
+                    offdiag = offdiag[sort_index]
+            else:
+                offdiag = (0.0,) * 3
+
+            cell, celldisp = construct_cell(diagdisp, offdiag)
+
+            # Handle pbc conditions
+            if len(tilt_items) == 3:
+                pbc_items = tilt_items
+            elif len(tilt_items) > 3:
+                pbc_items = tilt_items[3:6]
+            else:
+                pbc_items = ["f", "f", "f"]
+            pbc = ["p" in d.lower() for d in pbc_items]
+
+        if "ITEM: ATOMS" in line:
+            colnames = line.split()[2:]
+            datarows = [lines.popleft() for _ in range(n_atoms)]
+            data = np.loadtxt(datarows, dtype=str)
+            out_atoms = lammps_data_to_ase_atoms(
+                data=data,
+                colnames=colnames,
+                cell=cell,
+                celldisp=celldisp,
+                atomsobj=Atoms,
+                pbc=pbc,
+                **kwargs
+            )
+            images.append(out_atoms)
+
+        if len(images) > index_end >= 0:
+            break
+
+    return images[index]
+
+    
 if __name__ == '__main__':
   # relax system first
   eos = EOS(pair_coeff = '* * ffield.reax.lg C    H    O    N',
