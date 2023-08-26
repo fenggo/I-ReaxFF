bdb03453564d8dc9c29be6b9d89fc66bf0694178
FengGo <gfeng.alan@foxmail.com>
2023-08-26 12:16:51 +0000

    update tools/lmd.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/lmd.py
+++ tools/lmd.py
@@ -21,59 +21,21 @@ def nvt(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
               species=species,
               pair_coeff ='* * {:s} {:s}'.format(lib,sp),
               pair_style = 'reaxff control nn yes checkqeq yes',  # without lg set lgvdw no
-              fix = 'fix   1 all nve temp 300 300 100.0 ',
-              fix_modify = ' ',
-              more_commond = ' ',
-              thermo_style ='thermo_style  custom step temp epair etotal press vol cella cellb cellc cellalpha cellbeta cellgamma pxx pyy pzz pxy pxz pyz',
-              data='data.lammps',
-              restartfile='restart.eq')
-    print('\n-  running lammps nvt ...')
-    if n==1:
-       system('lammps<in.lammps>out')
-       # system('/home/feng/lammps/src/lmp_serial<in.lammps>out')
-    else:
-       system('mpirun -n {:d} lammps -i in.lammps>out'.format(n))
-    lammpstraj_to_ase('lammps.trj',inp='in.lammps')
-
-
-def opt(T=350,gen='siesta.traj',step=200,i=-1,l=0,c=0,
-        x=1,y=1,z=1,n=1,lib='reaxff_nn'):
-    atoms = read(gen,index=i)*(x,y,z)
-    # A = press_mol(A)
-    writeLammpsData(atoms,data='data.lammps',specorder=None, 
-                    masses={'Al':26.9820,'C':12.0000,'H':1.0080,'O':15.9990,
-                             'N':14.0000,'F':18.9980},
-                    force_skew=False,
-                    velocities=False,units="real",atom_style='charge')
-    if l==0:
-       runword='opti conv qiterative'
-    elif l==1:
-       runword= 'opti conp qiterative stre atomic_stress'
-
-    writeLammpsIn(log='lmp.log',timestep=0.1,total=200,restart=None,
-              pair_coeff ='* * ffield C H O N',
-              pair_style = 'reaxff control checkqeq yes',  # without lg set lgvdw no
               fix = 'fix   1 all nvt temp 300 300 100.0 ',
               fix_modify = ' ',
               more_commond = ' ',
               thermo_style ='thermo_style  custom step temp epair etotal press vol cella cellb cellc cellalpha cellbeta cellgamma pxx pyy pzz pxy pxz pyz',
               data='data.lammps',
-              restartfile='restart.eq')
-    print('\n-  running gulp optimize ...')
+              restartfile='restart')
+    print('\n-  running lammps nvt ...')
     if n==1:
-       system('/home/feng/mlff/lammps/src/lmp_serial<inp.lammps>out')
+       system('lammps<in.lammps>out')
     else:
-       system('mpirun -n {:d} /home/feng/mlff/lammps/src/lmp_serial<inp.lammps>out'.format(n))
-    # xyztotraj('his.xyz',mode=mode,traj='md.traj', checkMol=c,scale=False)
+       system('mpirun -n {:d} lammps -i in.lammps>out'.format(n))
+    lammpstraj_to_ase('lammps.trj',inp='in.lammps')
 
-def traj(inp='in.lammps'):
-    lammpstraj_to_ase('lammps.trj',inp=inp)
-
-def plot(out='out'):
-    get_lammps_thermal(logname='lmp.log',supercell=[1,1,1])
-
-def w(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
-      x=1,y=1,z=1,n=1,lib='ffield'):
+def npt(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
+        p=0.0,x=1,y=1,z=1,n=1,lib='ffield'):
     atoms = read(gen,index=i)*(x,y,z)
     symbols = atoms.get_chemical_symbols()
     species = sorted(set(symbols))
@@ -87,12 +49,74 @@ def w(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
               species=species,
               pair_coeff ='* * {:s} {:s}'.format(lib,sp),
               pair_style = 'reaxff control nn yes checkqeq yes',  # without lg set lgvdw no
-              fix = 'fix   1 all nvt temp {:f} {:f} 100.0 '.format(T,T),
+              fix = 'fix   1 all npt temp {:f} {:f} 100 iso {:f} {:f} 100'.format(T,T,p,p), 
               fix_modify = ' ',
               more_commond = ' ',
               thermo_style ='thermo_style  custom step temp epair etotal press vol cella cellb cellc cellalpha cellbeta cellgamma pxx pyy pzz pxy pxz pyz',
               data='data.lammps',
-              restartfile='restart.reax')
+              restartfile='restart')
+    print('\n-  running lammps npt ...')
+    if n==1:
+       system('lammps<in.lammps>out')
+    else:
+       system('mpirun -n {:d} lammps -i in.lammps>out'.format(n))
+    lammpstraj_to_ase('lammps.trj',inp='in.lammps')
+
+def opt(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
+        x=1,y=1,z=1,n=1,lib='ffield'):
+    atoms = read(gen,index=i)*(x,y,z)
+    symbols = atoms.get_chemical_symbols()
+    species = sorted(set(symbols))
+    sp      = ' '.join(species)
+    writeLammpsData(atoms,data='data.lammps',specorder=None, 
+                    masses={'Al':26.9820,'C':12.0000,'H':1.0080,'O':15.9990,
+                             'N':14.0000,'F':18.9980},
+                    force_skew=False,
+                    velocities=False,units="real",atom_style='charge')
+    writeLammpsIn(log='lmp.log',timestep=timestep,total=step,restart=None,
+              species=species,
+              pair_coeff ='* * {:s} {:s}'.format(lib,sp),
+              pair_style = 'reaxff control nn yes checkqeq yes',  # without lg set lgvdw no
+              fix = 'minimize	1e-5 1e-5 2000 2000', 
+              fix_modify = ' ',
+              more_commond = ' ',
+              thermo_style ='thermo_style  custom step temp epair etotal press vol cella cellb cellc cellalpha cellbeta cellgamma pxx pyy pzz pxy pxz pyz',
+              data='data.lammps',
+              restartfile='restart')
+    print('\n-  running lammps minimize ...')
+    if n==1:
+       system('lammps<in.lammps>out')
+    else:
+       system('mpirun -n {:d} lammps -i in.lammps>out'.format(n))
+    lammpstraj_to_ase('lammps.trj',inp='in.lammps')
+
+def traj(inp='in.lammps'):
+    lammpstraj_to_ase('lammps.trj',inp=inp)
+
+def plot(out='out'):
+    get_lammps_thermal(logname='lmp.log',supercell=[1,1,1])
+
+def w(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
+        x=1,y=1,z=1,n=1,lib='ffield'):
+    atoms = read(gen,index=i)*(x,y,z)
+    symbols = atoms.get_chemical_symbols()
+    species = sorted(set(symbols))
+    sp      = ' '.join(species)
+    writeLammpsData(atoms,data='data.lammps',specorder=None, 
+                    masses={'Al':26.9820,'C':12.0000,'H':1.0080,'O':15.9990,
+                             'N':14.0000,'F':18.9980},
+                    force_skew=False,
+                    velocities=False,units="real",atom_style='charge')
+    writeLammpsIn(log='lmp.log',timestep=timestep,total=step,restart=None,
+              species=species,
+              pair_coeff ='* * {:s} {:s}'.format(lib,sp),
+              pair_style = 'reaxff control nn yes checkqeq yes',  # without lg set lgvdw no
+              fix = 'fix   1 all nvt temp 300 300 100.0 ',
+              fix_modify = ' ',
+              more_commond = ' ',
+              thermo_style ='thermo_style  custom step temp epair etotal press vol cella cellb cellc cellalpha cellbeta cellgamma pxx pyy pzz pxy pxz pyz',
+              data='data.lammps',
+              restartfile='restart')
     print('\n-  write lammps input ...')
 
 if __name__ == '__main__':
@@ -108,6 +132,6 @@ if __name__ == '__main__':
        --T: MD simulation temperature
    '''
    parser = argparse.ArgumentParser()
-   argh.add_commands(parser, [opt,nvt,plot,traj,w])
+   argh.add_commands(parser, [opt,npt,nvt,plot,traj,w])
    argh.dispatch(parser)
 
