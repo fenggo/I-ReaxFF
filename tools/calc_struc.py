df4c6e789e2b6206cbbf4f1faa66e8447cf2a7fe
FengGo <gfeng.alan@foxmail.com>
2023-09-09 11:50:55 +0000

    update tools/calc_struc.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/calc_struc.py
+++ tools/calc_struc.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
+import numpy as np
 from os import getcwd,chdir,mkdir,system
 from os.path import exists
-import argh
-import argparse
 from ase.io import read
 from ase.io.trajectory import Trajectory
-from ase.data import chemical_symbols
-from irff.dft.siesta import siesta_md,siesta_opt,write_siesta_in
+from irff.dft.siesta import siesta_opt #, write_siesta_in
 
 
 def opt_structure(ncpu=8,T=2500,us='F',gen='poscar.gen',l=0,i=-1,step=200):
@@ -19,25 +17,38 @@ def opt_structure(ncpu=8,T=2500,us='F',gen='poscar.gen',l=0,i=-1,step=200):
     siesta_opt(A,ncpu=ncpu,us=us,VariableCell=vc,tstep=step,
                xcf='GGA',xca='PBE',basistype='split')
 
-def calc_strutures(structures,step=50,ncpu=8):
-    for atoms in structures:
-        siesta_opt(atoms,ncpu=ncpu,us='F',VariableCell='true',tstep=step,
-                   xcf='GGA',xca='PBE',basistype='split')
+def calc_strutures(traj,step=50,ncpu=8):
+    images = Trajectory(traj)
+    structures = range(190,239)
+   
+    root_dir   = getcwd()
+    with open('density.log','w') as fd:
+         print('# Crystal_id Density',file=fd)
+    for s in structures:
+        work_dir = root_dir+'/'+str(s)
+
+        if exists(s):
+           continue
+        else:
+           mkdir(s)
+
+        chdir(work_dir)
+        system('cp ../*.psf ./')
+        img = siesta_opt(images[s-1],ncpu=ncpu,us='F',VariableCell='true',tstep=step,
+                         xcf='GGA',xca='PBE',basistype='split')
+        system('mv siesta.out siesta-{:d}.out'.format(s))
+        system('rm siesta.* ')
+        atoms = img[-1]
+        masses = np.sum(atoms.get_masses())
+        volume = atoms.get_volume()
+        density = masses/volume/0.602214129
+        
+        chdir(root_dir)
+
+        with open('density.log','a') as fd:
+             print('{:5d} {:10.6f}'.format(s,density),file=fd)
 
 
 if __name__=='__main__': 
-   images = Trajectory('../../structures.traj')
-   structures = []
-   strucs     = []
-   cwd        = getcwd()
-
-   for s in structures:
-       if exists(s):
-          continue
-       else:
-          mkdir(s)
-
-       strucs.append(images[s])
-
-   calc_strutures(strucs,step=50,ncpu=8)
+   calc_strutures('../structures.traj',step=50,ncpu=8)
    
