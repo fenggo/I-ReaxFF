150d1b9f164e85531499caa275a4d6bea7bc07f4
FengGo <gfeng.alan@foxmail.com>
2023-09-10 13:49:00 +0000

    update tools/calc_struc.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/calc_struc.py
+++ tools/calc_struc.py
@@ -2,10 +2,58 @@
 import numpy as np
 from os import getcwd,chdir,mkdir,system
 from os.path import exists
+import sys
+import argparse
 from ase.io import read
 from ase.io.trajectory import Trajectory
 from irff.dft.siesta import siesta_opt #, write_siesta_in
 
+
+class Stack():
+    def __init__(self,entry=[]):
+        self.entry = entry
+        
+    def push(self,x):
+        self.entry.append(x) 
+
+    def pop(self):
+        return self.entry.pop()
+    
+    def close(self):
+        self.entry = None
+
+def read_individuals():
+    enthalpy  = []
+    gene      = {}
+    with open('../Individuals') as f:
+         for line in f.readlines():
+             st = Stack([])
+             for x in line:
+                if x!=']':
+                    st.push(x)
+                else:
+                    x_ = ' '
+                    while x_ !='[':
+                        x_ = st.pop()
+             line = ''.join(st.entry)
+             l = line.split()
+             
+             if len(l)>=10:
+                if l[0] != 'Gen':
+                   g = int(l[0])
+                   i = int(l[1])
+                   e = float(l[3])
+                   d = float(l[5])
+                   if g in gene:  
+                      gene[g].append((i,e,d))
+                   else:
+                      gene[g] = [(i,e,d)]
+                   # enthalpy.append(float(l[3]))
+         st.close()
+
+    k = gene.keys()
+    k_ = max(k)
+    return gene[k_]
 
 def opt_structure(ncpu=8,T=2500,us='F',gen='poscar.gen',l=0,i=-1,step=200):
     if exists('siesta.MDE') or exists('siesta.MD_CAR'):
@@ -17,14 +65,22 @@ def opt_structure(ncpu=8,T=2500,us='F',gen='poscar.gen',l=0,i=-1,step=200):
     siesta_opt(A,ncpu=ncpu,us=us,VariableCell=vc,tstep=step,
                xcf='GGA',xca='PBE',basistype='split')
 
-def calc_strutures(traj,step=50,ncpu=8):
+def calc_strutures(traj,density=1.88,ids=None,step=50,ncpu=8):
     images = Trajectory(traj)
-    structures = range(190,239)
-   
+    if ids is None:
+       ids = []
+
+       res = read_individuals()
+
+       for i,e,d in res:
+           if d>density:
+              ids.append(i)
+
     root_dir   = getcwd()
     with open('density.log','w') as fd:
          print('# Crystal_id Density',file=fd)
-    for s in structures:
+         
+    for s in ids:
         work_dir = root_dir+'/'+str(s)
 
         if exists(s):
@@ -50,5 +106,10 @@ def calc_strutures(traj,step=50,ncpu=8):
 
 
 if __name__=='__main__': 
-   calc_strutures('../structures.traj',step=50,ncpu=8)
+   parser = argparse.ArgumentParser(description='nohup ./train.py --v=1 --h=0> py.log 2>&1 &')
+   parser.add_argument('--d',default=0.0001,type=float, help='the density that big than this value')
+   args = parser.parse_args(sys.argv[1:])
+
+   ids = range(190,239)
+   calc_strutures('../structures.traj',ids=ids,step=30,ncpu=8)
    
