cf3fdff529d0b4c893a47df6fa64ac9da69e1c6c
FengGo <gfeng.alan@foxmail.com>
2023-10-01 08:27:52 +0000

    update irff/ml/fit.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/ml/fit.py
+++ irff/ml/fit.py
@@ -68,7 +68,7 @@ class Linear_be(object):
                ao = tf.sigmoid(tf.matmul(ai,self.m['fewo_'+bd]) + self.m['febo_'+bd])
 
             e_pred = -ao*self.j['p']['Desi_'+bd]*4.3364432032e-2
-            # loss+= tf.sqrt(tf.reduce_sum(tf.square((self.E[bd]-e_pred)*self.j['p']['Desi_'+bd]*4.3364432032e-2)))
+            # loss+= tf.reduce_sum(tf.square((self.E[bd]-e_pred)*self.j['p']['Desi_'+bd]*4.3364432032e-2))
             loss  += tf.nn.l2_loss((self.E[bd]-e_pred)*self.j['p']['Desi_'+bd]*4.3364432032e-2)
         return loss
 
