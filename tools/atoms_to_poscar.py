e52540da1e656bf0d5780e217bac38174a6a791d
FengGo <gfeng.alan@foxmail.com>
2023-10-13 07:37:31 +0000

    update tools/atoms_to_poscar.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/atoms_to_poscar.py
+++ tools/atoms_to_poscar.py
@@ -5,7 +5,7 @@ from ase.io import read
 from pymatgen.core import Structure
 from pymatgen.io.ase import AseAtomsAdaptor
 
-parser = argparse.ArgumentParser(description=help_)
+parser = argparse.ArgumentParser(description='./atoms_to_poscar.py --g=siesta.traj')
 parser.add_argument('--gen',default='md.traj', help='atomic configuration')
 parser.add_argument('--i',default=-1,type=int, help='the index in atomic configurations')
 args = parser.parse_args(sys.argv[1:])
