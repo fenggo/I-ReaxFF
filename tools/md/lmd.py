5db822e7e91ad9069ce1c660bb753b74cbe1c3ad
FengGo <gfeng.alan@foxmail.com>
2023-11-26 01:54:47 +0000

    update tools/md/lmd.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/md/lmd.py
+++ tools/md/lmd.py
@@ -90,6 +90,38 @@ def opt(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
        system('mpirun -n {:d} lammps -i in.lammps>out'.format(n))
     lammpstraj_to_ase('lammps.trj',inp='in.lammps')
 
+def msst(T=350,timestep=0.1,step=100,gen='poscar.gen',i=-1,mode='w',c=0,
+        x=1,y=1,z=1,n=1,
+        d='z',v=8.0,q=100,
+        lib='ffield',r='restart'):
+    atoms = read(gen,index=i)*(x,y,z)
+    symbols = atoms.get_chemical_symbols()
+    species = sorted(set(symbols))
+    sp      = ' '.join(species)
+    if r == 0:
+       r = None
+    writeLammpsData(atoms,data='data.lammps',specorder=None, 
+                    masses={'Al':26.9820,'C':12.0000,'H':1.0080,'O':15.9990,
+                             'N':14.0000,'F':18.9980},
+                    force_skew=False,
+                    velocities=False,units="real",atom_style='charge')
+    writeLammpsIn(log='lmp.log',timestep=timestep,total=step,restart=r,
+              species=species,
+              pair_coeff ='* * {:s} {:s}'.format(lib,sp),
+              pair_style = 'reaxff control nn yes checkqeq yes',  # without lg set lgvdw no
+              fix = 'fix msst all msst {:s} {:f} {:f} 100 mu 3e2 tscale 0.01 '.format(d,v,q),
+              fix_modify = ' ',
+              more_commond = ' ',
+              thermo_style ='thermo_style  custom step temp epair etotal press vol cella cellb cellc cellalpha cellbeta cellgamma pxx pyy pzz pxy pxz pyz',
+              data='data.lammps',
+              restartfile='restart')
+    print('\n-  running lammps nvt ...')
+    if n==1:
+       system('lammps<in.lammps>out')
+    else:
+       system('mpirun -n {:d} lammps -i in.lammps>out'.format(n))
+    lammpstraj_to_ase('lammps.trj',inp='in.lammps')
+
 def traj(inp='in.lammps',s=0,e=0):
     if e==0:
        atomid = None
@@ -136,6 +168,6 @@ if __name__ == '__main__':
        --T: MD simulation temperature
    '''
    parser = argparse.ArgumentParser()
-   argh.add_commands(parser, [opt,npt,nvt,plot,traj,w])
+   argh.add_commands(parser, [opt,npt,nvt,msst,plot,traj,w])
    argh.dispatch(parser)
 
