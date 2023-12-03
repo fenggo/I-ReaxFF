f1d05c35d313561f7d5c8f53e2691fc08fa19595
FengGo <gfeng.alan@foxmail.com>
2023-12-03 01:43:09 +0000

    add tools/plot/plot_thermal.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

---
+++ tools/plot/plot_thermal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_thermal():
+    plt.figure()
+    plt.ylabel(r'$Energy$ ($eV$)')
+    plt.xlabel(r'$Step$')
+    
+    vs = ['2.5','2.8','3.0']
+    cs = ['r','b','g']
+
+    for i,v in enumerate(vs):
+        fil   = 'themal-{:s}.log'.format(v)
+        data  = np.loadtxt(fil)
+        steps = data[:,0]
+        e     = data[:,2]
+    
+        # plt.scatter(ph[i],vh[i],marker = 'o', color = cmap.to_rgba(t), s=50, alpha=0.4)
+        plt.plot(steps,e,label=r'$energy .vs. step$', color=cs[i], linewidth=1.5, 
+                 linestyle='-')
+  
+    plt.legend()
+    plt.savefig('energy.pdf') 
+    plt.close()
+
+ 
+ 
+if __name__=='__main__':
+   plot_thermal()
+
