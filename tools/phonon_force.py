35e0b2153338280676f0a1f2ae7d970771f387a9
FengGo <gfeng.alan@foxmail.com>
2024-01-08 09:03:50 +0000

    update tools/phonon_force.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/phonon_force.py
+++ tools/phonon_force.py
@@ -4,9 +4,10 @@ from os import system
 import sys
 import argparse
 import numpy as np
-#from ase.io import read,write
+from ase.io import read # ,write
 from ase.visualize import view
 from irff.md.gulp import get_gulp_forces
+from irff.md.lammps import get_lammps_forces
 from irff.dft.siesta import parse_fdf,parse_fdf_species,write_siesta_in
 from irff.irff import IRFF
 
@@ -14,18 +15,20 @@ from irff.irff import IRFF
 parser = argparse.ArgumentParser(description='stretch molecules')
 parser.add_argument('--n',default=1,type=int, help='displacement number')
 parser.add_argument('--f',default='in.fdf',type=str, help='fdf file name')
+parser.add_argument('--g',default='POSCAR',type=str, help='geomentry file name')
 args    = parser.parse_args(sys.argv[1:])
 
 # system('cp geo.genS-00{:d} geo-s{:d}.gen'.format(args.n,args.n))
-# atoms = read('geo.gen')
-# write_siesta_in(atoms, coord='cart',md=False, opt='CG',
-#                 VariableCell='true', xcf='VDW', xca='DRSLL',
-#                 basistype='DZP')
+atoms = read(args.g)
+write_siesta_in(atoms, coord='cart',md=False, opt='CG',
+                VariableCell='true', xcf='VDW', xca='DRSLL',
+                basistype='DZP')
 
 spec  = parse_fdf_species(fdf='in.fdf')
 atoms = parse_fdf('supercell-00{:d}'.format(args.n),spec=spec)
 #view(atoms)
 
-get_gulp_forces([atoms],wforce=True)
+# get_gulp_forces([atoms],wforce=True)
+get_lammps_forces([atoms],wforce=True)
 system('mv Forces.FA Forces-00{:d}.FA'.format(args.n))
 
