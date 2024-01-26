15561cfb3846ff4a188ae132b43f5dd3a193eafe
FengGo <gfeng.alan@foxmail.com>
2024-01-26 11:30:02 +0000

    update tools/md/lopt.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/md/lopt.py
+++ tools/md/lopt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import os
+import sys
+import argparse
 import numpy as np
 from ase import Atom, Atoms
 from ase.io import read
@@ -12,6 +14,13 @@ from ase.io.trajectory import Trajectory
 
 os.environ['ASE_LAMMPSRUN_COMMAND'] = 'mpirun -n 4 lammps'
 
+parser = argparse.ArgumentParser(description='./atoms_to_poscar.py --g=siesta.traj')
+parser.add_argument('--g',default='md.traj', help='atomic configuration')
+parser.add_argument('--i',default=-1,type=int, help='the index in atomic configurations')
+parser.add_argument('--x',default=1,type=int, help='the supercell size in x')
+parser.add_argument('--y',default=1,type=int, help='the supercell size in y')
+parser.add_argument('--z',default=1,type=int, help='the supercell size in z')
+args = parser.parse_args(sys.argv[1:])
 
 # cmds = ['pair_style    quip',
 #         'pair_coeff * * ../Carbon_GAP_20_potential/Carbon_GAP_20.xml \"\" 6']
@@ -24,12 +33,7 @@ files = ['Carbon_GAP_20_potential/Carbon_GAP_20.xml',
          'Carbon_GAP_20_potential/Carbon_GAP_20.xml.sparseX.GAP_2020_4_27_60_2_50_5_4363']
 
 ### super cell defination
-x = 4   
-y = 4
-z = 1
-
-G = read('POSCAR.unitcell')*(x,y,z)
-
+G = read(args.g)*(args.x,args.y,args.z)
 
 #lammps= LAMMPSlib(lmpcmds=cmds, log_file='graphene.log')
 lammps = LAMMPS(files=files)
@@ -47,20 +51,20 @@ opt = BFGS(sf)
 traj = Trajectory('path.traj', 'w', G)
 opt.attach(traj)
 
-opt.run(0.001)
+opt.run(0.00001)
 
 traj = Trajectory('path.traj')
 atoms = traj[-1]
 traj.close()
 
-if x>1 or y>1 or z>1:   # get unit cell
-   ncell     = x*y*z
+if args.x>1 or args.y>1 or args.z>1:   # get unit cell
+   ncell     = args.x*args.y*args.z
    natoms    = int(len(atoms)/ncell)
    species   = atoms.get_chemical_symbols()
    positions = atoms.get_positions()
    forces    = atoms.get_forces()
    cell      = atoms.get_cell()
-   cell      = [cell[0]/x, cell[1]/y,cell[2]/z]
+   cell      = [cell[0]/args.x, cell[1]/args.y,cell[2]/args.z]
    u         = np.linalg.inv(cell)
    pos_      = np.dot(positions[0:natoms], u)
    posf      = np.mod(pos_, 1.0)          # aplling simple pbc conditions
