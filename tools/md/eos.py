1f3d9adcca96b01e40c05ea78d2b0ec83a2fc3f2
FengGo <gfeng.alan@foxmail.com>
2024-02-16 13:41:47 +0000

    update tools/md/eos.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/md/eos.py
+++ tools/md/eos.py
@@ -2,13 +2,12 @@
 # coding: utf-8
 import numpy as np
 import copy
-from ase.visualize import view
 from ase.io import read
-from ase.io.trajectory import TrajectoryWriter,Trajectory
-from irff.structures import structure
+from ase.io.trajectory import TrajectoryWriter #,Trajectory
+from ase.calculators.singlepoint import SinglePointCalculator
 from irff.molecule import Molecules,enlarge # SuperCell,moltoatoms
 #from irff.md.lammps import writeLammpsData
-
+from irff.irff_np import IRFF_NP
 
 A = read('hmxc.gen')
 x = A.get_positions()
@@ -19,22 +18,20 @@ A.set_positions(x_)
 m_  = Molecules(A)
 nmol = len(m_)
 
+ir = IRFF_NP(atoms=A,
+             libfile='ffield.json',
+             nn=True)
+
 print('\nnumber of molecules:',nmol)
 
-ff = [0.9,0.92,0.94,0.96,0.98,1.0,1.02,1.04,1.06,1.08,1.1]
-# ff = [1.02]
+ff = [0.92,0.94,0.96,0.98,1.0,1.02,1.04,1.06,1.08,1.1,1.12]
 cell = A.get_cell()
 
 with TrajectoryWriter('md.traj',mode='w') as his:
     for f in ff:
         m = copy.deepcopy(m_)
         _,A = enlarge(m,cell=cell,fac=f,supercell=[1,1,1])
-        # natom = len(A)
-        # print('\nnumber of atoms:',natom)
-        # nmol = len(m)
-        # print('\nnumber of molecules in super-cell:',nmol)
-        # A.write('tatb_super.gen')
+        ir.calculate(A)
+        A.calc = SinglePointCalculator(A,energy=ir.E)
         his.write(atoms=A)
-    # view(A)
- 
  
