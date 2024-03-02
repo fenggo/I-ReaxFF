52bc86d0db79db93ae119d096e5c16ae3a36540c
FengGo <gfeng.alan@foxmail.com>
2024-03-02 05:03:13 +0000

    test
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/molecule.py
+++ irff/molecule.py
@@ -289,7 +289,9 @@ def Molecules(atoms,rcut=None,check=False):
     natm,atoms,X,table = get_neighbors(Atoms=atoms,
                                        r_cut=rcut,
                                        cell=cell) #,exception=['O-O','H-H']
-    print(table)
+    for i,tab in enumerate(table):
+        print(i,tab)
+        
     m = molecules(natm,atoms,
                   cell=cell,
                   table=table,
