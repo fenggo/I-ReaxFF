b7e673e97725543806a3080bb15c40379f782868
FengGo <gfeng.alan@foxmail.com>
2024-03-02 05:51:59 +0000

    test
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/molecule.py
+++ irff/molecule.py
@@ -463,10 +463,11 @@ class molecule(object):
               bd   = self.atom_name[iatom]+'-'+self.atom_name[jatom]
               vr   = self.mol_x[jatom] - self.mol_x[iatom]
               r_   = np.sqrt(np.sum(vr*vr,axis=0))
+              print(iatom,jatom,r_,r)
               if r_>self.rcut[bd]: 
                  self.mol_x[jatom] = self.mol_x[iatom] + self.vr[iatom][jatom]
                  r   = np.sqrt(np.sum(self.vr[iatom][jatom]*self.vr[iatom][jatom],axis=0))
-                 print(iatom,jatom,r_,r)
+                 # print(iatom,jatom,r_,r)
 
   def move_atom(self,i,j):
       vr  = self.mol_x[j] - self.mol_x[i]
