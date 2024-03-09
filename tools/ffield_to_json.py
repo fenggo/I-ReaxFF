a41644ec04e58070cba2572adfbe85c4aedd4f40
FengGo <gfeng.alan@foxmail.com>
2024-03-09 03:18:48 +0000

    update tools/ffield_to_json.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/ffield_to_json.py
+++ tools/ffield_to_json.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
+import numpy as np
+import json as js
+from os import environ,system
+from ase.io import read
 from irff.reaxfflib import read_ffield,write_lib
 # from irff.irnnlib_new import write_lib
 from irff.qeq import qeq
-from ase.io import read
-import argh
-import argparse
-import json as js
-from os import environ,system
 
 
 def ffieldtojson():
@@ -14,6 +13,10 @@ def ffieldtojson():
 
     fj = open('ffield.json','w')
     # j = {'p':p,'m':[],'bo_layer':[],'zpe':[]}
+    for key in p:
+        # print(p[key])
+        if isinstance(p[key],np.float32):
+           p[key]= float(p[key])
     j = {'p':p,'m':None,
          'EnergyFunction':0,
          'VdwFunction':0,
