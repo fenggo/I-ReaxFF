75b86af4a705e5aba4894ab209396b3db0d78190
FengGo <gfeng.alan@foxmail.com>
2024-03-09 03:07:58 +0000

    update tools/ffield_to_json.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/ffield_to_json.py
+++ tools/ffield_to_json.py
@@ -32,31 +32,6 @@ def ffieldtojson():
     fj.close()
 
 
-def init_bonds(p_):
-    spec,bonds,offd,angs,torp,hbs = [],[],[],[],[],[]
-    for key in p_:
-        # key = key.encode('raw_unicode_escape')
-        # print(key)
-        k = key.split('_')
-        if k[0]=='bo1':
-           bonds.append(k[1])
-        elif k[0]=='rosi':
-           kk = k[1].split('-')
-           # print(kk)
-           if len(kk)==2:
-              if kk[0]!=kk[1]:
-                 offd.append(k[1])
-           elif len(kk)==1:
-              spec.append(k[1])
-        elif k[0]=='theta0':
-           angs.append(k[1])
-        elif k[0]=='tor1':
-           torp.append(k[1])
-        elif k[0]=='rohb':
-           hbs.append(k[1])
-    return spec,bonds,offd,angs,torp,hbs
-
-
 if __name__ == '__main__':
    ffieldtojson()
 
