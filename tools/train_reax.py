28434317b46dbd3fa437678defe8a70e142fa624
FengGo <gfeng.alan@foxmail.com>
2024-03-09 07:11:52 +0000

    update tools/train_reax.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/train_reax.py
+++ tools/train_reax.py
@@ -6,145 +6,131 @@ matplotlib.use('Agg')
 from os import system, getcwd, chdir,listdir
 from os.path import isfile # exists
 import sys
-import argh
 import argparse
 from irff.reax import ReaxFF
 from irff.mpnn import MPNN
 from irff.dingtalk import send_msg
-from irff.initCheck import Init_Check
-from irff.data.ColData import ColRawData
+from irff.data.ColData import ColData
 from irff.dft.CheckEmol import check_emol
-from irff.tools.fluctuation import bo_fluct,make_fluct
+from irff.ml.fluctuation import morse
 
-parser = argparse.ArgumentParser(description='Molecular Dynamics Simulation')
-parser.add_argument('--step',default=50000,type=int, help='training steps')
+parser = argparse.ArgumentParser(description='nohup ./train.py --v=1 --h=0> py.log 2>&1 &')
+parser.add_argument('--step',default=30000,type=int, help='training steps')
 parser.add_argument('--lr',default=0.0001,type=float, help='learning rate')
 parser.add_argument('--writelib',default=1000,type=int, help='every this step to write parameter file')
-parser.add_argument('--print',default=10,type=int,help='every this step to print')
-parser.add_argument('--loss',default=300,type=float,help='the convergence criter of loss')
-parser.add_argument('--convergence',default=0.95,type=float,help='the convergence criter of accuracy')
-parser.add_argument('--maxcycle',default=1,type=int,help='the max training cycle')
+parser.add_argument('--pr',default=10,type=int,help='every this step to print')
+parser.add_argument('--pi',default=1,type=int,help='regularize BO pi component')
+parser.add_argument('--loss',default=1.0,type=float,help='the convergence criter of loss')
+parser.add_argument('--convergence',default=0.999,type=float,help='the convergence criter of accuracy')
+parser.add_argument('--maxepoch',default=100,type=int,help='the max training epoch')
 parser.add_argument('--batch',default=50,type=int,help='the batch size of every configuration')
-parser.add_argument('--boc',default=1,type=int,help='F(f1,f2,f3,f4,f5)  optimize flag')
-parser.add_argument('--bo',default=1,type=int,help='bond-order parameters optimize flag')
+parser.add_argument('--t',default=0,type=int,help='optimize the three-boday term flag')
+parser.add_argument('--h',default=0,type=int,help='optimize the hydrogen bond term flag')
+parser.add_argument('--a',default=0,type=int,help='surpvise the angle term flag')
+parser.add_argument('--f',default=0,type=int,help='optimize the four-boday term flag')
+parser.add_argument('--bo',default=1,type=int,help='optimize the bond term flag')
+parser.add_argument('--zpe',default=0,type=int,help='optimize the zero point energy')
+parser.add_argument('--vdw',default=1,type=int,help='optimize the vdw energy')
 args = parser.parse_args(sys.argv[1:])
-getdata = ColRawData()
-
-rawdata = {'Fe2O3_1':'mdout/Fe2O3_1.traj',
-           'Fe3O4_1':'mdout/Fe3O4_1.traj',
-           'Fe2O3_1':'mdout/Fe2O3_1.traj',
-           'H2O_1':'mdout/H2O_1.traj',
-           'H2_1':'mdout/H2_1.traj',}
-check_emol(rawdata)
-dataset = getdata(batch=100,rawdata=rawdata)
-
-data  = dataset.keys()
-data_ = random.sample(data,ntraj)
-dataset_ = {}
-
-for key in data_:
-    dataset_[key] = dataset[key]
 
 
-def train_reax(writelib=10000,print_step=100,
-              step=50000,opt=None,lr=1.0e-4,
-              convergence=0.97,lossConvergence=100.0,batch=50):
-    cons = ['val','vale','lp3','cutoff','boc1']
-    if not args.boc:
-       cons += ['valboc','boc1','boc2','boc3','boc4','boc5']
-    if not args.bo:
-       cons += ['bo1','bo2','bo3','bo4','bo5','bo6','rosi','ropi','ropp']
-
-    # belo,beup,vlo,vup = make_fluct(fluct=0.2,bond=['C-C'],csv='fluct')
-
-    rn = ReaxFF(libfile='ffield.json',
-                dataset=dataset,
-                dft='siesta',
-                optword='nocoul',
-                opt=None,cons=cons,
-                clip={'boc1':(10.0,30.0),'boc2':(3.0,9.0),'boc3':(0.1,19.9),'boc4':(0.5,9.9),
-                      'bo1':(-0.4,-0.02),'bo2':(4.0,15.0),
-                      'bo3':(-0.4,-0.02),'bo4':(4.0,16.0),
-                      'bo5':(-0.4,-0.02),'bo6':(4.0,16.0),
-                      'Desi':(10.0,160.0),'Depi':(30.0,90.0),'Depp':(30.0,100.0),
-                      'be1':(0.01,0.5),'be2':(0.01,0.2),
-                      'ovun1':(0.1,0.9),'ovun3':(0.01,30.0),'ovun4':(0.5,10.0),
-                      'ovun5':(0.1,50.0),'ovun7':(0.1,20.0),'ovun8':(1.0,18.0),
-                      'lp1':(5.0,18.0),'lp2':(0.0,20.0),
-                      'Devdw':(0.025,0.2),'alfa':(10.0,14.0),'rvdw':(1.9,2.3),'vdw1':(1.5,2.0),'gammaw':(2.5,5.0),
-                      'val2':(0.21,2.0),'val3':(0.1,5.0),
-                      'val9':(1.0,2.0),
-                      'pen1':(9.0,11.0),'pen2':(1.0,9.0),'pen3':(0.0,1.0),'pen4':(1.0,6.0),
-                      'coa1':(-1.0,0.0),'cot1':(-1.0,0.0),
-                      'V2':(0.0,10.0),'V3':(0.0,10.0),'V1':(0.0,10.0)},
-                batch_size=batch,
-                losFunc='n2',
-                spv_vdw=False,# vup=vup,#vlo=vlo,
-                lambda_bd=10000.0,
-                lambda_me=0.01,
-                atol=0.002,hbtol=0.002,
-                weight={'al4':2.0,'others':2.0},
-                convergence=convergence,
-                lossConvergence=lossConvergence) # Loss Functon can be n2,abs,mse,huber
-
-    # GradientDescentOptimizer AdamOptimizer AdagradOptimizer RMSPropOptimizer
-    loss,accu,accMax,i,zpe =rn.run(learning_rate=lr,
-                      step=step,
-                      print_step=print_step,
-                      writelib=writelib)
-    libstep = int(i - i%writelib)
-
-    if i==libstep:
-       libstep = libstep - writelib
-    if libstep<=0:
-       ffd = 'ffield.json'
-    else:
-       ffd = 'ffield_' + str(libstep) +'.json'
-
-    if loss==9999999.9 and accu==-1.0:
-       send_msg('-  Warning: the loss is NaN, please check parameters!')
-       return 0.0,1.0,1.0,None,None,i
-
-    p   = rn.p_
-    ME = rn.MolEnergy_
-
-    rn.close()
-    return loss,accu,accMax,p,ME,i
+getdata = ColData()
+ 
+strucs = ['h2o2','ch4w2','h2o16']  
 
 
-def run(step=50000,convergence=0.99,loss_conver=20.0,writelib=1000,
-        opt=None,
-        print_step=10,
-        lr=1.0e-4,
-        maxcycle=20,
-        batch=50):
-    accu,loss = 0.0,100
-    c = 0
+weight={'h2o2':2.0,
+        'others':2.0}
 
-    while accu<convergence and c<maxcycle:
-          loss,accu,accMax,p,zpe,i=train_reax(step=step,opt=opt,
-                                              convergence=convergence,lr=lr,
-                                              writelib=writelib,print_step=print_step)
-          if loss>loss_conver and accu>convergence:
-             convergence = accu + 0.0003
-          c += 1
+dataset = {'h22-v':'aimd_h22/h22-v.traj',
+           }
+batchs  = {'h2o16':50,'others':args.batch}
+for mol in strucs:
+    b = batchs[mol] if mol in batchs else batchs['others']
+    trajs = getdata(label=mol,batch=b)
+    dataset.update(trajs)
 
-    if loss>0.0:
-       send_msg('-  Convergence reached, the loss %7.4f and accuracy %7.4f.' %(loss,accu))
+clip = {'Desi':(125.0,750.0),
+        'bo1':(-0.077,-0.02),'bo2':(5.0,8.77),
+        'bo3':(-0.077,-0.02),'bo4':(5.0,8.77), 
+        'bo5':(-0.067,-0.028),'bo6':(5.0,8.77), 
+        'rosi_C':(1.2,1.50),'rosi_H-N':(0.8,1.11),
+        'rosi_N':(1.08,1.52),
+        'rosi_C-H':(1.0,1.15),'ropi_C-H':(1.0,1.15),
+        'rosi_C-N':(1.0,1.41),'ropi_C-N':(1.0,1.21),'ropp_C-N':(1.0,1.21),
+        'rosi_H-O':(0.82,1.11),'ropi_H-O':(0.81,1.11),'ropp_H-O':(0.81,1.11),
+        'rvdw_C-N':(1.69,2.4),'rvdw_C':(1.757,2.34),'rvdw_H':(1.53,2.0),'rvdw_H-N':(1.6,2.2),
+        'rvdw_N':(1.74,2.2),'rvdw_O-N':(1.775,2.35),'rvdw_H-O':(1.6,2.0),'rvdw_O':(1.73,2.2),
+        'rvdw_C-O':(1.79,2.4),
+        'Devdw':(0.01,1.0),
+        'alfa':(6.0,16.0),
+        'vdw1':(0.8,8.0),'gammaw':(1.5,12.0),'gammaw_C':(5.09,12.0),'gammaw_H':(4.25,12.0),'gammaw_N':(7.025,12.0),
+        'valang_N':(0.0,4.8),'valang_C':(0.0,4.8),'valang_H':(0.0,2.7),'valang_O':(0.0,4.8),
+        'val1':(0.0,60.0),'val1_C-C-O':(0.0,0.0),'val1_H-N-O':(0.0,0.0),'val1_H-O-N':(0.0,0.0),
+        'val2':(0.5,2.0),'val4':(0.5,2.0),'val5':(0.0,5.6),
+        'tor2':(1.0,6.6),'tor4':(0.0004,2.0),
+        'V2':(-42.0,64.0),#'V3_N-C-N-N':(0.0,0.0),
+        'V1_C-C-C-H':(0.0,0.0),'V2_C-C-C-H':(0.0,0.0),'V3_C-C-C-H':(0.0,0.0),
+        'V1_N-C-C-N':(0.0,0.0),'V3_N-C-C-N':(0.0,0.0),
+        'rohb':(1.68,4.0),'hb1':(-1.6,9.0),'hb2':(0.0,26.0),
+        'acut':(0.0001,0.01)}
 
+cons = ['ovun2','ovun3','ovun4','ovun1',
+        'ovun6','ovun7','ovun8','ovun5',
+        # 'rosi','ropi','ropp',
+        'lp2','lp1',
+        #'theta0',
+        #'val9','valang',
+        #'val10','val8',
+        'valboc','cot1','cot2','coa1','coa2','coa3','coa4',
+        'pen1','pen2','pen3','pen4',
+        #'vdw1','gammaw','rvdw','alfa','Devdw',
+        #'rohb','Dehb','hb1','hb2',
+        'val1_C-C-O','val1_H-N-O','val1_H-O-N',
+        'V1_X-N-N-X','V2_X-N-N-X','V3_X-N-N-X',
+        'V1_X-C-N-X','V2_X-C-N-X','V3_X-C-N-X',
+        #'V1_N-C-N-N','V2_N-C-N-N','V3_N-C-N-N',
+        #'V1_C-C-N-N','V2_C-C-N-N','V3_C-C-N-N',
+        'val','lp3','cutoff',#'acut',
+        ]    # 不进行局域优化
 
+if not args.f:
+   cons += [ 'tor1','tor2','tor3','tor4',      # Four-body
+             'V1','V2','V3',]
+if not args.t:
+   cons += ['val1','val2','val3','val6','val7'] #
+if not args.a:
+   #cons += ['theta0']
+   cons += ['theta0','val9','val10','val8','vale','valang','val4','val5'] # 
+if not args.vdw:
+   cons += ['vdw1','gammaw','rvdw','alfa','Devdw']
+if not args.h:
+   cons += ['rohb','Dehb','hb1','hb2']
+
+if not args.bo:
+   cons += ['Depi','Depp','Desi',
+            'rosi','ropi','ropp',
+            'bo1','bo2','bo3','bo4','bo5','bo6',
+            'vdw1','gammaw','rvdw','alfa','Devdw',
+            ] ### 
+   mpopt = [0,0,0,0] # neural network for BO,MF,BE,VDW
+
+       
 if __name__ == '__main__':
-   ''' use commond like ./train.py --s=50000 --l=0.0001 to run it
-                     or ./train.py to use default parameters
-       s:   steps
-       l:   learning rate
-   '''
-
-   run(step=args.step,
-       convergence=args.convergence,
-       loss_conver=args.loss,
-       lr=args.lr,
-       print_step=args.print,
-       batch=args.batch,
-       maxcycle=args.maxcycle,
-       writelib=args.writelib)
+   ''' train ''' 
+   rn = ReaxFF(dataset=dataset,            
+             weight=weight,
+             optword='nocoul',
+             cons=cons,clip=clip,
+             lambda_reg=0.001,lambda_bd=100.0,lambda_me=0.001,lambda_pi=0.003,
+             bdopt=None,    # ['H-N'], 
+             batch_size=args.batch,
+             fixrcbo=False,
+             losFunc='n2',  # n2, mse, huber,abs
+             convergence=0.95) # Loss Functon can be n2,abs,mse,huber
+   
+   rn.run(learning_rate=args.lr,
+          step=args.step,
+          print_step=args.pr,
+          writelib=args.writelib,
+          method='AdamOptimizer')
