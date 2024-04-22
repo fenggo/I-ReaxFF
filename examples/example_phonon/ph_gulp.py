b55d15bf4dd21ad0032c7814d4c1e0d594f02fe6
FengGo <gfeng.alan@foxmail.com>
2024-04-22 01:45:57 +0000

    update examples/example_phonon/ph_gulp.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- examples/example_phonon/ph_gulp.py
+++ examples/example_phonon/ph_gulp.py
@@ -9,9 +9,8 @@ system('phonopy --siesta -c=in.fdf -d --dim="8 8 1" --amplitude=0.02')
 system('./phonon_force.py --n=1')
 # system('cp force.0 lammps_forces_gp.0')
 
-
 system('phonopy -f Forces-001.FA --siesta')
-system('phonopy -c gp-unit.yaml -p --dim="6 6 1" --band="0.0 0.0 0.0 1/4 0.0 0.0  0.5 0.0 0.0  2/3 -1/3 1/2 1/3 -1/6 0.0  0.0 0.0 0.0"')
+system('phonopy --siesta -c in.fdf -p --dim="6 6 1" --band="0.0 0.0 0.0 1/4 0.0 0.0  0.5 0.0 0.0  2/3 -1/3 1/2 1/3 -1/6 0.0  0.0 0.0 0.0"')
 
 system('phonopy-bandplot --gnuplot band.yaml > band.dat')
 
