ad5373d7a51b242a6c22678b8ba30b46d4cb2928
FengGo <gfeng.alan@foxmail.com>
2024-06-23 15:19:21 +0000

    update test/test_reax_force.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- test/test_reax_force.py
+++ test/test_reax_force.py
@@ -14,6 +14,7 @@ from irff.irff_autograd import IRFF
 from irff.data.ColData import ColData
 from irff.dft.CheckEmol import check_emol
 import matplotlib.pyplot as plt
+from irff.md.gulp import get_gulp_forces
 
 
 ir = ReaxFF_nn_force(dataset={'md':'md.traj'},
@@ -28,13 +29,33 @@ for s in ir.bop:
 print('\n---- irff ----\n')
 images = Trajectory('md.traj')
 ir_ = IRFF(atoms=images[0],libfile='ffield.json',nn=True)
+ir2 = IRFF_NP(atoms=images[0],libfile='ffield.json',nn=True)
+
+forces = images[0].get_forces()
+
 
 for i,img in enumerate(images):
     ir_.calculate(atoms=img)
-    print(ir_.E,ir.E[s][i].item())
+    ir2.calculate(atoms=img)
+    print('--     IR     --      RForce     --     IRNP     --' )
+    print(ir_.E,ir.E[s][i].item(),ir2.E)
+    print(ir_.Eover.item(),ir.eover[s][i].item(),ir2.Eover)
+    print(ir_.Eunder.item(),ir.eunder[s][i].item(),ir2.Eunder)
+    print(ir_.Eang.item(),ir.eang[s][i].item(),ir2.Eang)
+    # print('\n IR-dpi \n',ir2.Dpil)
  
-# for i,img in enumerate(images):
+ 
+print('\n----  forces  ----\n')
 ir_.calculate(atoms=images[0])
-print(ir_.results['forces'] ,ir.force[s][0])
+for i in range(ir_.natom):
+    print(ir_.results['forces'][i],'----' ,ir.force[s][0][i].detach().numpy(),
+             '----',forces[i])
 
+# get_gulp_forces(images)
+# print('\n lammps: \n')
+# images = Trajectory('md.traj')
+# atoms  = images[0]
+# forces = atoms.get_forces()
+# for f in forces:
+#     print(f)
 
