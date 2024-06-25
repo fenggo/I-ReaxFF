8f715ca9fe5103db51aad96052e35e29a1078206
FengGo <gfeng.alan@foxmail.com>
2024-06-25 01:12:36 +0000

    train with force
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- test/train_with_force.py
+++ test/train_with_force.py
@@ -2,12 +2,25 @@
 import torch
 from ase import Atoms
 from irff.reax_force import ReaxFF_nn_force
- 
+from irff.data.ColData import ColData
 
+getdata = ColData()
 
-rn = ReaxFF_nn_force(dataset={'gp4':'gp4.traj'},
+dataset = {}
+strucs = ['gp4',#'gp5',#'gpp'
+          ]
+
+weight={'c2h4':100.0,'gphit3':4.0,'cnt5-1':5.0,'nomb':0.0,'others':2.0}
+batchs  = {'others':100}
+
+for mol in strucs:
+    b = batchs[mol] if mol in batchs else batchs['others']
+    trajs = getdata(label=mol,batch=b)
+    dataset.update(trajs)
+
+rn = ReaxFF_nn_force(dataset={'gp4':'data/gp4-0.traj'},
                      weight_energy={'others':1.0},
-                     weight_force={'others':0.01},
+                     weight_force={'others':1.0},
                      libfile='ffield.json')
 # rn.forward()
 
@@ -17,9 +30,9 @@ rn = ReaxFF_nn_force(dataset={'gp4':'gp4.traj'},
 # for p in rn.parameters():
 #     print(p)
 # print(rn.opt)
-optimizer = torch.optim.Adam(rn.parameters(), lr=0.0001 )
+optimizer = torch.optim.Adam(rn.parameters(), lr=0.00001 )
  
-iter_num = 100
+iter_num = 10000
 
 
 for step in range(iter_num):
@@ -29,6 +42,8 @@ for step in range(iter_num):
     loss.backward(retain_graph=True)
     optimizer.step()
 
-    print( "{:8d} loss: {:20.5f} energy: {:20.5f} force: {:20.5f}".format(step,
+    print( "{:8d} loss: {:10.5f}   energy: {:20.5f}   force: {:10.5f}".format(step,
                 loss.item(),rn.loss_e.item(),rn.loss_f.item()))
- 
+    if step%1000==0:
+       rn.save_ffield('ffield_{:d}.json'.format(step))
+
