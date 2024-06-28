c142968a90fa05df83879592175e84c2f4422609
FengGo <gfeng.alan@foxmail.com>
2024-06-28 11:27:31 +0000

    save ffield
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/reax_force.py
+++ irff/reax_force.py
@@ -1421,21 +1421,25 @@ class ReaxFF_nn_force(nn.Module):
           unit  = self.unit if key in self.punit else 1.0
           # value = float(self.p[k].item()/unit)
           # print(k,' = ',value)
-        #   if key in ['V1','V2','V3','tor1','cot1']:
-        #      k_ = k.split('_')[1]
-        #      if k_ not in self.torp:
-        #         continue
-        #      self.p_[k] = value
-          if key in self.p_offd:
-             b = k.split('_')[1]
-             s = b.split('-')
-             if s[0]==s[1]:
-                self.p_[key+'_'+s[0]]          = value
-                self.p_[key+'_'+s[0]+'-'+s[1]] = value
-             else:
-                self.p_[k] = value
-          else:
+          if key in ['V1','V2','V3','tor1','cot1']:
+             k_ = k.split('_')[1]
+             if k_ not in self.torp:
+                continue
              self.p_[k] = value
+          if key in self.punit:
+             self.p_[k] = float(self.p[k].item()/self.unit)
+          else:
+             self.p_[k] = float(self.p[k].item())
+        #   if key in self.p_offd:
+        #      b = k.split('_')[1]
+        #      s = b.split('-')
+        #      if s[0]==s[1]:
+        #         self.p_[key+'_'+s[0]]          = value
+        #         self.p_[key+'_'+s[0]+'-'+s[1]] = value
+        #      else:
+        #         self.p_[k] = value
+        #   else:
+        #      self.p_[k] = value
         # if k in self.ea_var:
         #    self.p_[k] = self.ea_var[k]
         # else:
