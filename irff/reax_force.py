0f57a1d3a8535e28b8fde96cf07117857e72603f
FengGo <gfeng.alan@foxmail.com>
2024-06-28 04:59:27 +0000

    angle cutoff
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/reax_force.py
+++ irff/reax_force.py
@@ -346,7 +346,7 @@ class ReaxFF_nn_force(nn.Module):
       self.bopi[st]   = self.Hpi[st][-1]
       self.bopp[st]   = self.Hpp[st][-1]
 
-      self.bo[st]     = torch.relu(self.bo0[st] - self.atol)
+      self.bo[st]     = torch.relu(self.bo0[st] - self.p['acut'])
 
       bso             = []
       bo0             = self.bo0[st][:,self.bdid[st][:,0],self.bdid[st][:,1]]
@@ -366,7 +366,7 @@ class ReaxFF_nn_force(nn.Module):
       self.Delta_pi[st]= self.bopi[st]+self.bopp[st]
       self.delta_pi[st]= torch.sum(self.Delta_pi[st],2) 
 
-      self.fbot[st]   = taper(self.bo0[st],rmin=self.atol,rmax=2.0*self.atol) 
+      self.fbot[st]   = taper(self.bo0[st],rmin=self.p['acut'],rmax=2.0*self.p['acut']) 
       self.fhb[st]    = taper(self.bo0[st],rmin=self.hbtol,rmax=2.0*self.hbtol) 
 
   def get_bondorder(self,st,Dbi,H,Dbj,Hsi,Hpi,Hpp):
@@ -773,7 +773,7 @@ class ReaxFF_nn_force(nn.Module):
       return f_11
 
   def get_four_conj(self,tor,boij,bojk,bokl,w,s_ijk,s_jkl,fijkl):
-      exptol= torch.exp(-self.p['cot2']*torch.square(self.atol - 1.5))
+      exptol= torch.exp(-self.p['cot2']*torch.square(self.p['acut'] - 1.5))
       expij = torch.exp(-self.p['cot2']*torch.square(boij-1.5))-exptol
       expjk = torch.exp(-self.p['cot2']*torch.square(bojk-1.5))-exptol 
       expkl = torch.exp(-self.p['cot2']*torch.square(bokl-1.5))-exptol
@@ -992,7 +992,8 @@ class ReaxFF_nn_force(nn.Module):
           grad = True if key in self.opt else False
           self.p[key] = nn.Parameter(torch.tensor(self.p_[key]*unit_), 
                                      requires_grad=grad)
-      self.atol         = torch.clamp(self.p['acut'],min=self.p_['acut']*0.96)        # atol
+      # self.atol         = torch.clamp(self.p['acut'],min=self.p_['acut']*0.96)        # atol
+      self.p['acut'].data = torch.clamp(self.p['acut'].data,min=self.p_['acut']*0.96)
 
       for key in self.p_spec:
           unit_ = self.unit if key in self.punit else 1.0
