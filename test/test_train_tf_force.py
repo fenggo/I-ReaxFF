2538a04b9d8c0a48190a18298ebb5d10db8a411b
FengGo <gfeng.alan@foxmail.com>
2024-07-01 00:16:36 +0000

    update test/test_train_tf_force.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- test/test_train_tf_force.py
+++ test/test_train_tf_force.py
@@ -6,17 +6,6 @@ from irff.data.ColData import ColData
 
 getdata = ColData()
 
-dataset = {}
-strucs = ['gp4',#'gp5',#'gpp'
-          ]
-
-weight={'c2h4':100.0,'gphit3':4.0,'cnt5-1':5.0,'nomb':0.0,'others':2.0}
-batchs  = {'others':100}
-
-for mol in strucs:
-    b = batchs[mol] if mol in batchs else batchs['others']
-    trajs = getdata(label=mol,batch=b)
-    dataset.update(trajs)
 
 rn = ReaxFF_nn(dataset={'gp4':'data/gp4-0.traj'},
                libfile='ffield.json',
