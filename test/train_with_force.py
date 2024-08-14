a96ee0f10b780b256bbf28e60a4a6369db9cdcdb
FengGo <gfeng.alan@foxmail.com>
2024-08-14 05:23:32 +0000

    update test/train_with_force.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- test/train_with_force.py
+++ test/train_with_force.py
@@ -1,53 +1,87 @@
 #!/usr/bin/env python
+from os import  system
+import json as js
 import torch
 from ase import Atoms
 from irff.reax_force import ReaxFF_nn_force
 from irff.data.ColData import ColData
+from irff.reax_force_data import reax_force_data
+from irff.intCheck import init_bonds,check_tors
 
 getdata = ColData()
 
 dataset = {}
-strucs = ['gp4',#'gp5',#'gpp'
-          ]
+strucs = ['cf11']
 
-weight={'c2h4':100.0,'gphit3':4.0,'cnt5-1':5.0,'nomb':0.0,'others':2.0}
-batchs  = {'others':100}
+batchs  = {'others':50}
 
 for mol in strucs:
     b = batchs[mol] if mol in batchs else batchs['others']
     trajs = getdata(label=mol,batch=b)
     dataset.update(trajs)
 
-rn = ReaxFF_nn_force(dataset={'gp4':'data/gp4-0.traj'},
-                     weight_energy={'others':1.0},
-                     weight_force={'others':1.0},
-                     libfile='ffield.json')
-# rn.forward()
+data = {}
 
-# param = rn.get_parameter(rn)
-# print(param)
-# print(list(rn.named_parameters()))
-# for p in rn.parameters():
-#     print(p)
-# print(rn.opt)
-optimizer = torch.optim.Adam(rn.parameters(), lr=0.00001 )
- 
-iter_num = 10000
+with open('ffield.json','r') as lf:
+     j = js.load(lf)
 
+spec,bonds,offd,angs,torp,hbs = init_bonds(j['p'])
+tors = check_tors(spec,torp)
 
-for step in range(iter_num):
-    E,F = rn()
-    loss = rn.get_loss()
-    optimizer.zero_grad()
+for st in dataset:
+    data_ = reax_force_data(structure=st,
+                            traj=dataset[st],
+                            vdwcut=10.0,
+                            rcut=j['rcut'],
+                            rcuta=j['rcutBond'],
+                            batch=1000,
+                            variable_batch=True,
+                            m=j['m'],
+                            mf_layer=j['mf_layer'],
+                            p=j['p'],spec=spec,bonds=bonds,
+                            angs=angs,tors=tors,
+                            hbs=hbs,
+                            screen=True)
+    data[st] = data_
 
-    if step%1000==0:
-       rn.save_ffield('ffield_{:d}.json'.format(step))
-       
-    loss.backward(retain_graph=True)
-    optimizer.step()
+# rn.cuda()
+# rn.compile()
 
-    print( "{:8d} loss: {:10.5f}   energy: {:10.5f}   force: {:10.5f}".format(step,
-                loss.item(),rn.loss_e.item(),rn.loss_f.item()))
-    if step%1000==0:
-       rn.save_ffield('ffield_{:d}.json'.format(step))
+iter_num = 101
 
+for eproch in range(iter_num):
+    for st in data:
+        data_ = {st:data[st]}
+        rn = ReaxFF_nn_force(data=data_,
+                            weight_energy={'others':1.0},
+                            weight_force={'others':1.0},
+                            cons=['acut'],
+                            tors=tors,
+                            libfile='ffield.json',
+                            screen=True,
+                            lambda_bd=1000.0,
+                            lambda_pi=0.0,
+                            lambda_reg=0.01,
+                            lambda_ang=0.0,
+                            device='cuda')
+        optimizer = torch.optim.Adam(rn.parameters(), lr=0.0001 )
+        natom = 0
+        for st in rn.strcs:
+            natom += rn.batch[st]*rn.natom[st]
+        for step in range(10):
+            E,F  = rn()
+            loss = rn.get_loss()
+            optimizer.zero_grad()
+            print( "eproch: {:5d} step {:5d} loss: {:10.5f} energy: {:10.5f} force: {:10.5f} pen: {:10.5f}".format(eproch,
+                            step,loss.item(),rn.loss_e.item()/natom,rn.loss_f.item()/natom,rn.loss_penalty.item()))
+            loss.backward(retain_graph=True)
+            optimizer.step()
+        rn.save_ffield('ffield.json')
+        rn.close()
+
+    if eproch%100==0:
+       # rn.save_ffield('ffield_{:d}.json'.format(step))
+       rn.save_ffield('ffield.json')
+       # system('cp ffield.json ffield_{:d}.json'.format(step))
+
+# rn.save_ffield('ffield.json')
