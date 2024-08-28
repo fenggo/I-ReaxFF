331fad63086d1d556e3b2cbc1bef901852b876ac
FengGo <gfeng.alan@foxmail.com>
2024-08-28 07:24:07 +0000

    clean up
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/ml/train.py
+++ irff/ml/train.py
@@ -141,7 +141,7 @@ def train(step=5000,print_step=100,writelib=500,
            # ub=1.1*np.max(X_,axis=0)
            de = Evolution(func,n_dim=X_.shape[1], F=0.5,size_pop=size_pop+size_,
                           scale=_scale,max_iter=max_generation, n_clusters=n_clusters,
-                          prob_mut=prob_mut,X_input=X_)    
+                          prob_mut=prob_mut,X_input=X_) 
 
            best_x,best_y = de.run(logfile=galog)                     ###   PSO GMM
            print('  The guessed score of best candidate: {:f} '.format(float(-best_y)),file=galog) 
