623c51d7961aaa514d6a155ab2f409ea9f233274
FengGo <gfeng.alan@foxmail.com>
2024-09-18 13:32:23 +0000

    update tools/init_ffield_fit.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/init_ffield_fit.py
+++ tools/init_ffield_fit.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
-from distutils.command.build import build
-from os.path import isfile
-from os import system
 import sys
-import argh
+#import argh
 import argparse
 import json as js
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-from scipy.optimize import minimize_scalar
+#import pandas as pd
+#import matplotlib
+#import matplotlib.pyplot as plt
+#from scipy.optimize import minimize_scalar
 from irff.data.ColData import ColData
-from irff.ml.data import get_data,get_bond_data,get_md_data
+from irff.ml.data import get_data,get_bond_data # ,get_md_data
 from irff.ml.fit import train
 tf.compat.v1.disable_eager_execution()
 
 
-def bo(i=4,j=1,traj='md.traj',bonds=None):
-    D, Bp, B, R, E = get_bond_data(i,j,images=None, traj=traj,bonds=bonds)
-    for i,bp in enumerate(Bp):
-        print(i,R[i],D[i][1],np.sum(B[i]),E[i])
-    print('\n r & E:',R[i],E[i])
-    print('\n B\': \n',Bp[i])
-    print('\n D: \n',D[i])
-    print('\n B: \n',B[i])
+def get_bond_energy(p,bd,bond_data):
+    ''' compute bond-energy '''
+    unit = 4.3364432032e-2
+    bsi  = bond_data[:,0]
+    bpi  = bond_data[:,1]
+    bpp  = bond_data[:,2]
+    
+    powb = np.power(np.where(bsi>0.00000001,bsi,0.00000001),p['be2_'+bd])
+    expb = np.exp(p['be1_'+bd],(1.0-powb))
+    #print(p['Desi_'+bd])
+    e_si = p['Desi_'+bd]*bsi*expb*unit
+    e_pi = p['Depi_'+bd]*bpi*unit
+    e_pp = p['Depp_'+bd]*bpp*unit
+    e    = (e_si+e_pi+e_pp)/(p['Desi_'+bd]*unit)
+    return e
 
 
 def fit(step=1000,obj='BO'):
-    unit =  4.3364432032e-2
-    Desi = 424.95
+    with open('ffield.json','r') as lf:
+         j  = js.load(lf)
+         p = j['p']
     dataset = {'al60-0': 'data/al64-0.traj',
                'al60-1': 'data/al64-1.traj',
                'AlO-0': 'data/AlO-0.traj',
@@ -52,11 +57,11 @@ def fit(step=1000,obj='BO'):
     B_ = {}
     for bd in B:
         bp     = np.array(Bp[bd])
+        e      = np.array(E[bd])
         # print(bp)
-        bo     = bp[:,1]
-        m_     = max(bo)
-        E_[bd] = bo*0.875/m_
-        B_[bd] = bp*np.array([0.45,0.425,0.4])
+        # bo     = bp[:,1]
+        B_     = np.array(B[bd])
+        E_[bd] = get_bond_energy(p,bd,B_)
 
     train(Bp,D,B_,E_,bonds=bonds,step=step,fitobj=obj)
 
