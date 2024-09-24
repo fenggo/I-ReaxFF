aca297b6c2c4d42c00e86775fa234448cd7dbbdf
FengGo <gfeng.alan@foxmail.com>
2024-09-24 05:52:49 +0000

    random_init
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/ml/fit.py
+++ irff/ml/fit.py
@@ -43,14 +43,16 @@ class Linear_be(object):
             self.De[bd]        = tf.Variable(self.j['p']['Desi_'+bd]*self.unit,name='Desi_'+bd)
             # self.De_[bd]     = tf.clip_by_value(self.De[bd],0.0,999*self.unit)
             # self.De[bd]      = tf.constant(self.j['p']['Desi_'+bd]*self.unit,name='Desi_'+bd)
-            # self.m['fewi_'+bd] = tf.Variable(self.j['m']['fewi_'+bd],name='fewi_'+bd)
-            # self.m['febi_'+bd] = tf.Variable(self.j['m']['febi_'+bd],name='febi_'+bd)
-            # self.m['fewo_'+bd] = tf.Variable(self.j['m']['fewo_'+bd],name='fewo_'+bd)
-            # self.m['febo_'+bd] = tf.Variable(self.j['m']['febo_'+bd],name='febo_'+bd)
-            self.m['fewi_'+bd] = tf.Variable(tf.random.normal([3,l[0]],stddev=0.1),name='fewi_'+bd)
-            self.m['febi_'+bd] = tf.Variable(tf.random.normal([l[0]],stddev=0.1),name='febi_'+bd)
-            self.m['fewo_'+bd] = tf.Variable(tf.random.normal([l[0],1],stddev=0.1),name='fewo_'+bd)
-            self.m['febo_'+bd] = tf.Variable(tf.random.normal([1],stddev=0.1),name='febo_'+bd)
+            if random_init:
+               self.m['fewi_'+bd] = tf.Variable(tf.random.normal([3,l[0]],stddev=0.1),name='fewi_'+bd)
+               self.m['febi_'+bd] = tf.Variable(tf.random.normal([l[0]],stddev=0.1),name='febi_'+bd)
+               self.m['fewo_'+bd] = tf.Variable(tf.random.normal([l[0],1],stddev=0.1),name='fewo_'+bd)
+               self.m['febo_'+bd] = tf.Variable(tf.random.normal([1],stddev=0.1),name='febo_'+bd)
+            else:
+               self.m['fewi_'+bd] = tf.Variable(self.j['m']['fewi_'+bd],name='fewi_'+bd)
+               self.m['febi_'+bd] = tf.Variable(self.j['m']['febi_'+bd],name='febi_'+bd)
+               self.m['fewo_'+bd] = tf.Variable(self.j['m']['fewo_'+bd],name='fewo_'+bd)
+               self.m['febo_'+bd] = tf.Variable(self.j['m']['febo_'+bd],name='febo_'+bd)
             self.m['few_'+bd]  = []
             self.m['feb_'+bd]  = []
             for i in range(hidelayer):
@@ -58,13 +60,21 @@ class Linear_be(object):
                 # b = np.array(self.j['m']['few_'+bd][i])
                 # m,n = w.shape
                 # print('hidden layer:',i,m,n)
-                self.m['few_'+bd].append(tf.Variable(tf.random.normal([l[0],l[0]],stddev=0.1),
-                                         name='fewh_'+bd))
-                self.m['feb_'+bd].append(tf.Variable(tf.random.normal([l[0]],stddev=0.1),
-                                         name='febh_'+bd))
+
                 if i+1 > self.j['be_layer'][1]:
+                   self.m['few_'+bd].append(tf.Variable(self.j['m']['few_'+bd][-1],name='fewh_'+bd))
+                   self.m['feb_'+bd].append(tf.Variable(self.j['m']['feb_'+bd][-1],name='febh_'+bd))
                    self.j['m']['few_'+bd].append(self.j['m']['few_'+bd][-1])
                    self.j['m']['feb_'+bd].append(self.j['m']['feb_'+bd][-1])
+                else:
+                   if random_init:
+                      self.m['few_'+bd].append(tf.Variable(tf.random.normal([l[0],l[0]],stddev=0.1),
+                                               name='fewh_'+bd))
+                      self.m['feb_'+bd].append(tf.Variable(tf.random.normal([l[0]],stddev=0.1),
+                                               name='febh_'+bd))
+                   else:
+                      self.m['few_'+bd].append(tf.Variable(self.j['m']['few_'+bd][i],name='fewh_'+bd))
+                      self.m['feb_'+bd].append(tf.Variable(self.j['m']['feb_'+bd][i],name='febh_'+bd))
 
 
         if hidelayer > self.j['be_layer'][1]:
