4a1ee18ffe44c46e5a82e1f1df968582dd06e924
FengGo <gfeng.alan@foxmail.com>
2025-01-07 06:52:44 +0000

    add tools/uspex/sort_poscars.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

---
+++ tools/uspex/sort_poscars.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+with open('POSCARS','r') as f:
+     lines = f.readlines()
+
+with open('POSCARS','w') as f:
+     card = False
+     for i,line in enumerate(lines):
+         if line.find('direct')>=0 or line.find('Direct')>=0:
+            card = True
+		 elif line.find('EA')>=0 and line.find('Sym.group')>=0:
+		    card = True
+         if card and line.find('direct')<0 and line.find('Direct')<0:
+		    l = line.split()
+            print(l[0],l[1],l[2],file=f)
+         else:
+            print(line[:-1],file=f)
