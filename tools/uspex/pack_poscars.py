582f29fc13210bba7156b9757a5d38cfe87b61c3
FengGo <gfeng.alan@foxmail.com>
2025-02-13 13:27:12 +0000

    update tools/uspex/pack_poscars.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/uspex/pack_poscars.py
+++ tools/uspex/pack_poscars.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 from os import getcwd,chdir,listdir,system
 from os import getcwd,listdir
+from os.path import exists
 from ase.io import read
 from pymatgen.io.ase import AseAtomsAdaptor
 from ase.io import read
 # from irff.molecule import press_mol
 
-density = 1.85
+density = 1.81
 cdir    = getcwd()
 
 cdir_   = '/'.join(cdir.split('/')[:-1])
@@ -14,8 +15,9 @@ cdir_   = '/'.join(cdir.split('/')[:-1])
 dirs    = listdir(cdir_)
 for dir_ in dirs:
     d = dir_.split('-')
-    if d[0]=='results11' and d[1].isalpha():
-       with open('{:s}/{:s}/density.log'.format(cdir_,dir_),'r') as f:
+    if d[0]=='results13' and d[1].isalpha(): # d[1].isalpha(): isalnum()
+       if exists('{:s}/{:s}/density.log'.format(cdir_,dir_)):
+          with open('{:s}/{:s}/density.log'.format(cdir_,dir_),'r') as f:
            for i,line in enumerate(f.readlines()):
                if i==0:
                   continue
@@ -24,9 +26,16 @@ for dir_ in dirs:
                id_ = l[0]
                if den>=density:
                   print(id_,den)
+                  if exists('{:s}/{:s}/{:s}/id_{:s}.traj'.format(cdir_,dir_,id_,id_)):
+                     atoms = read('{:s}/{:s}/{:s}/id_{:s}.traj'.format(cdir_,dir_,id_,id_))
+                  elif exists('{:s}/{:s}/{:s}/individual_{:s}.traj'.format(cdir_,dir_,id_,id_)):
+                     atoms = read('{:s}/{:s}/{:s}/individual_{:s}.traj'.format(cdir_,dir_,id_,id_))
+                  else:
+                     atoms = read('{:s}/{:s}/{:s}/md_{:s}.traj'.format(cdir_,dir_,id_,id_))
+                  atoms.write('{:s}/{:s}/{:s}/POSCAR.{:s}'.format(cdir_,dir_,id_,id_))
                   system('cp {:s}/{:s}/{:s}/POSCAR.{:s} POSCAR.{:s}{:s}'.format(cdir_,dir_,id_,id_,d[1],id_))
     
-############ pack to poscars ##########
+########### pack to poscars ##########
 files   = listdir(cdir)
 poscars = []
 
@@ -65,5 +74,7 @@ for p in poscars:
                    angles[0],angles[1],angles[2]),file=fposcars)
         else:
            print(line[:-1],file=fposcars)
+
+    # print('{:s}'.format(i_))
 fposcars.close()
 
