7ef0156adc28a8e58e8b4d99608fffb982b277fc
FengGo <gfeng.alan@foxmail.com>
2025-02-23 01:51:50 +0000

    update tools/uspex/ebind_gulp.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/uspex/ebind_gulp.py
+++ tools/uspex/ebind_gulp.py
@@ -8,12 +8,10 @@ import json as js
 from os import system
 from ase.io import read
 from ase.io.trajectory import TrajectoryWriter #,Trajectory
-from ase.calculators.singlepoint import SinglePointCalculator
 from irff.molecule import Molecules,enlarge # SuperCell,moltoatoms
 #from irff.md.lammps import writeLammpsData
 from irff.irff_np import IRFF_NP
 from irff.molecule import press_mol
-from irff.md.gulp import write_gulp_in,get_reax_energy
 from irff.md.gulp import opt
 
 ''' scale the crystal box, while keep the molecule structure unchanged
@@ -50,19 +48,20 @@ ir = IRFF_NP(atoms=A,
 
 print('\nnumber of molecules:',nmol)
 print('\ndensity of configuration:',density)
-ff = [1.0,4.0] #,1.9 ,2.0,2.5,3.0,3.5,4.0
+ff = [1.0,5.0] #,1.9 ,2.0,2.5,3.0,3.5,4.0
 cell = A.get_cell()
 e  = []
 eg = []
 
 with TrajectoryWriter('md.traj',mode='w') as his:
-    for f in ff:
+    for i,f in enumerate(ff):
         m = copy.deepcopy(m_)
         _,A = enlarge(m,cell=cell,fac=f,supercell=[1,1,1])
         ir.calculate(A)
         e.append(ir.E)
 
         A = opt(atoms=A,step=1000,l=0,t=0.0000001,n=args.n, x=1,y=1,z=1)
+        system('mv md.traj md_{:d}.traj'.format(i))
         e_ = A.get_potential_energy()
         eg.append(e_)
         his.write(atoms=A)
