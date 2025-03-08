9611eeb903baec7f7c7e38bee27a0fbf839b2e0a
FengGo <gfeng.alan@foxmail.com>
2025-03-08 02:48:04 +0000

    update tools/cif_to_poscar.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/cif_to_poscar.py
+++ tools/cif_to_poscar.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import sys
+import argparse
 import pymatgen.core as pmg
 # Integrated symmetry analysis tools from spglib
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
+parser = argparse.ArgumentParser(description='eos by scale crystal box')
+parser.add_argument('--g', default='Individuals.traj',type=str, help='trajectory file')
+args = parser.parse_args(sys.argv[1:])
 
-structure = pmg.Structure.from_file("251401.cif")
+structure = pmg.Structure.from_file(args.g)
 
 finder = SpacegroupAnalyzer(structure)
 sp = finder.get_space_group_symbol()
