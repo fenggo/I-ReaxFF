c625a4b95f0c27aa2bc7179382a9a2a8a6c545aa
FengGo <gfeng.alan@foxmail.com>
2025-03-14 04:32:28 +0000

    update tools/uspex/plot_individuals.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/uspex/plot_individuals.py
+++ tools/uspex/plot_individuals.py
@@ -5,103 +5,75 @@ from ase import Atoms
 import matplotlib.pyplot as plt
 import numpy as np
 from irff.irff_np import IRFF_NP
+from irff.tools.load_individuals import load_density_energy
 
+I,D,E,O = load_density_energy('Individuals')
+lab,e,d = [],[],[]
 
-class Stack():
-    def __init__(self,entry=[]):
-        self.entry = entry
-        
-    def push(self,x):
-        self.entry.append(x) 
+with open('exp.dat','r') as f:
+     for line in f.readlines():
+         l = line.split()
+         if len(l)>0:
+            lab.append(l[0])
+            e.append(float(l[1]))
+            d.append(float(l[2]))
 
-    def pop(self):
-        return self.entry.pop()
+ops     = {}
+density = {}
+enthalpy= {}
+for i,op in enumerate(O):
+    if op not in density:
+        density[op]  = [D[i]]
+        enthalpy[op] = [E[i]]
+    else:
+        density[op].append(D[i])
+        enthalpy[op].append(E[i])
+
+plt.figure()
+plt.ylabel(r'$Enthalpy$ ($eV$)',fontdict={'size':10})
+plt.xlabel(r'$Density$ ($g/cm^3$)',fontdict={'size':10})
+
+markers = {'Heredity':'o','keptBest':'s','softmutate':'^',
+            'Rotate':'v','Permutate':'8','Random':'p'}
+colors  = {'Heredity':'#1d9bf7','keptBest':'#9933fa','softmutate':'#00ffff',
+            'Rotate':'#be588d','Permutate':'#35a153','Random':'#00c957'}
+r_p = {'520':0.02,'560':0.02,'556':0.02}
+hide= []
+for op in density:
+    # if op in hide:
+    #    continue
+    plt.scatter(density[op],np.array(enthalpy[op]),alpha=0.9,
+            marker=markers[op],color='none',
+            edgecolor=colors[op],s=50,
+            label=op)
     
-    def close(self):
-        self.entry = None
+for l_,e_,d_ in zip(lab,e,d):
+    c = 'k'
+    if l_=='epsilon':
+       lb = r'$\varepsilon$-CL-20'
+       c  = 'r'
+    elif l_=='gamma':
+       lb = r'$\gamma$-CL-20'
+       c = '#4169e1' #0787c3
+    elif l_=='beta':
+       lb = r'$\beta$-CL-20'
+       c = '#2ec0c2'
+    plt.scatter(d_,e_,alpha=0.9,
+            marker='*',color='none',
+            edgecolor=c,s=120,
+            label=lb)
+    plt.text(d_+0.03,e_,lb,ha='center',color=c,
+             fontweight='bold',fontsize=6)
 
+for i,t in enumerate(I):
+    d = D[i]
+    e = E[i]
+    if t in r_p:
+       plt.text(d+r_p[t],e,t,ha='center',fontsize=6)
+    else:
+       plt.text(d,e+0.15,t,ha='center',fontsize=6)
 
-def plot_indiv(findi='Individuals'):
-    ids       = []
-    id_       = []
-    gene      = {}
-    density   = []
-    dens      = []
-    id_g      = {}
-    dens_g    = {}
-    ent_g     = {}
-    op_g      = {}
-
-    with open(findi) as f:
-         for line in f.readlines():
-             st = Stack([])
-             for x in line:
-                if x!=']':
-                    st.push(x)
-                else:
-                    x_ = ' '
-                    while x_ !='[':
-                        x_ = st.pop()
-             line = ''.join(st.entry)
-             l = line.split()
-             
-             if len(l)>=10:
-                if l[0] != 'Gen':
-                   g = l[0]
-                   i = int(l[1])
-                   e = float(l[3])
-                   d = float(l[5])
-                   s = l[6]
-                   if s=='N/A' or s=='100000.000':
-                      continue
-                   if g in gene:  
-                      gene[g].append(d)
-                      id_g[g].append(i)
-                      dens_g[g].append(d)
-                      ent_g[g].append(e)
-                      op_g[g].append(l[2])
-                   else:
-                      gene[g]    = [d]
-                      id_g[g]    = [i]
-                      dens_g[g]  = [d]
-                      ent_g[g]   = [e]
-                      op_g[g]    = [l[2]]
-         st.close()
-    
-    ng      = str(len(gene))
-    ops     = {}
-    density = {}
-    enthalpy= {}
-    for i,op in enumerate(op_g[ng]):
-        if op not in density:
-           density[op]  = [dens_g[ng][i]]
-           enthalpy[op] = [ent_g[ng][i]]
-        else:
-           density[op].append(dens_g[ng][i])
-           enthalpy[op].append(ent_g[ng][i])
-
-    plt.figure()
-    plt.ylabel(r'$Enthalpy$ ($eV$)',fontdict={'size':10})
-    plt.xlabel(r'$Density$ ($g/cm^3$)',fontdict={'size':10})
-
-    markers = {'Heredity':'o','keptBest':'s','softmutate':'^',
-               'Rotate':'v','Permutate':'p','Random':'8'}
-    colors  = {'Heredity':'#1d9bf7','keptBest':'#c65861','softmutate':'#ffa725',
-               'Rotate':'#be588d','Permutate':'#35a153','Random':'#f26a11'}
-    for op in density:
-        plt.scatter(density[op],np.array(enthalpy[op])+90,alpha=0.9,
-                marker=markers[op],color=colors[op],
-                edgecolor=colors[op],s=25,
-                label=op,
-                )
-    
-    plt.legend(loc='best',edgecolor='yellowgreen') # loc = lower left upper right best
-    plt.savefig('individuals.pdf',transparent=True) 
-    plt.close() 
-
-
-if __name__ == '__main__':
-   ''' use commond like ./tplot.py to run it'''
-   plot_indiv(findi='Individuals')
-
+plt.legend(loc='best',edgecolor='yellowgreen') # loc = lower left upper right best
+plt.savefig('individuals.svg',transparent=True) 
+plt.close() 
 
