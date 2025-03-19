ddb82ef5b6a3526d5ead50df8ef07c5d1c5d6008
FengGo <gfeng.alan@foxmail.com>
2025-03-19 22:25:59 +0000

    update tools/rotator.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/rotator.py
+++ tools/rotator.py
@@ -3,7 +3,10 @@ import numpy as np
 from ase.io import read
 from irff.molecule import press_mol
 
-''' scale the crystal box, while keep the molecule structure unchanged
+'''  
+  rotae matrix
+     cos_x   -sin_x
+     sin_x    cos_x
 '''
 
 
@@ -15,19 +18,16 @@ x_ = x - m
 
 natom = len(A)
 
-'''
-rotae matrix
-     cos_x   -sin_x
-     sin_x    cos_x
-
-'''
-
-rotater = np.array([[[-1.0,0.0,0.0],
-                    [ 0.0,1.0,0.0],
-                    [ 0.0,0.0,-1.0] ]])
+rotater = np.array([[[1.0,0.0,0.0],
+                     [ 0.0,0.0,-1.0],
+                     [ 0.0,1.0,0.0] ]])
 
 x_ = np.matmul(x_.reshape([natom,1,3]),rotater)
 print(x_.shape)
 
+cell  = A.get_cell()
+cell_ = np.matmul(cell.reshape([3,1,3]),rotater)
+
 A.set_positions(x_.reshape([natom,3]))
+A.set_cell(cell_.reshape([3,3]))
 A.write('POSCAR')
