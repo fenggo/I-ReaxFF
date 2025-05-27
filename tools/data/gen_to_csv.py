a95fc7f0952addbb79b45e8f9cf04fc0a6f2b9ae
FengGo <gfeng.alan@foxmail.com>
2025-05-27 09:39:04 +0000

    update tools/data/gen_to_csv.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/data/gen_to_csv.py
+++ tools/data/gen_to_csv.py
@@ -6,29 +6,35 @@ from ase.io.trajectory import Trajectory
 from ase.io import read
 from pymatgen.io.ase import AseAtomsAdaptor
 from ase.io import read
-from irff.deb.compare_energies import deb_gulp_energy
+# from irff.deb.compare_energies import deb_gulp_energy
+from irff.md.gulp import write_gulp_in,get_reax_energy
 # from irff.molecule import press_mol
 
 cdir    = getcwd()
 files   = listdir(cdir)
 poscars = [gen for gen in files if gen.endswith('.gen') ]
 images  = []
-
-for gen in poscars:
-    gen_ = gen.split('.')[0]
-    atoms = read(gen)
-    images.append(atoms.copy())
-
-(e,ebond_,eunder_,eover_,elone_,eang_,etcon_,epen_,
-    etor_,efcon_,ev,ehb,ec) = deb_gulp_energy(images, ffield='reaxff_nn')
-n_images = len(images)
+ncpu    = 1
 
 f = open('train.csv','w')
 print(',material_id,energy,density,hydrogen_bond_energy,cif,volume',file=f)
 
-for i in tqdm(range(len(images))):
-    atoms = images[i]
-    gen_ = poscars[i].split('.')[0]
+for i in tqdm(range(len(poscars))):
+    gen   = poscars[i]
+    gen_  = gen.split('.')[0]
+    atoms = read(gen)
+    images.append(atoms.copy())
+
+    write_gulp_in(atoms,runword='gradient nosymmetry conv qite verb',
+                      lib='reaxff_nn.lib')
+    if ncpu==1:
+       system('gulp<inp-gulp>out')
+    else:
+       system('mpirun -n {:d} gulp<inp-gulp>out'.format(ncpu))
+
+    (e,eb_,el_,eo_,eu_,ea_,ep_,
+     etc_,et_,ef_,ev_,ehb,ecl_,esl_)= get_reax_energy(fo='out')
+
     structure = AseAtomsAdaptor.get_structure(atoms)
     structure.to(filename="POSCAR")
 
@@ -39,9 +45,8 @@ for i in tqdm(range(len(images))):
     structure = AseAtomsAdaptor.get_structure(atoms)
     structure.to(filename="{:s}.cif".format(gen_))
     
-    
     print('{:d},{:s},{:f},{:f},{:f},\"'.format(i,gen_,
-                       e[i],density,ehb[i]),end='',file=f)
+                       e,density,ehb),end='',file=f)
     with open("{:s}.cif".format(gen_),'r') as fc:
          for line in fc.readlines():
              print(line.strip(),file=f)
