f0fd1e3c5862156b7d74cd21ac387c1ac9017dd8
FengGo <gfeng.alan@foxmail.com>
2025-06-06 23:19:51 +0000

    update test/test_reax_force.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- test/test_reax_force.py
+++ test/test_reax_force.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from ase.io.trajectory import Trajectory,TrajectoryWriter
-from irff.reax_force import ReaxFF_nn_force
+from irff.reaxff_torch import ReaxFF_nn as ReaxFF_nn_torce
+from irff.reax_nn import ReaxFF_nn
 from irff.irff_np import IRFF_NP
 from irff.irff_autograd import IRFF
 from irff.data.ColData import ColData
@@ -8,16 +9,49 @@ import matplotlib.pyplot as plt
 from irff.md.gulp import get_gulp_forces
 
 
-ir = ReaxFF_nn_force(dataset={'md':'md.traj'},
-                     screen=True,
-                     libfile='ffield.json')
-ir.forward()
+ir = ReaxFF_nn_torce(dataset={'md':'md.traj'},
+               weight_energy={'others':1.0},
+               weight_force={'md':1},
+               cons=['acut'],
+               libfile='ffield.json',
+               screen=True,
+               lambda_bd=100.0,
+               lambda_pi=0.0,
+               lambda_reg=0.001,
+               lambda_ang=0.0 )
+ir.forward('md')
 
-print('\n---- reax_nn_force ----\n')
-for s in ir.bop:
-    print('\n evdw \n',ir.evdw[s])
-    print('\n ehb \n',ir.ehb[s])  
-    print(ir.Evdw[s].shape)
+ir1 = ReaxFF_nn(dataset={'md':'md.traj'},
+               weight_force={'md':1},
+               MessageFunction=3,
+               mf_layer=[9,2],
+               be_layer=[9,1],
+               screen=True,
+               libfile='ffield.json')
+ir1.initialize()
+ir1.session()  
+
+print('\n---- reaxff_nn ----\n')
+for s in ir1.bop:
+    E,ebond,elone,eover,eunder,eang,etor,etcon,epen,efcon,evdw,ehb,ecoul = ir1.sess.run([ir1.E[s],
+                                          ir1.ebond[s],
+                                          ir1.elone[s],ir1.eover[s],ir1.eunder[s],
+                                          ir1.eang[s],ir1.etor[s],ir1.etcon[s],ir1.epen[s],
+                                          ir1.efcon[s],
+                                          ir1.evdw[s],ir1.ehb[s],
+                                          ir1.ecoul[s]],feed_dict=ir1.feed_dict)
+
+    bo = ir1.sess.run(ir1.bo0[s],feed_dict=ir1.feed_dict)
+
+    ir1.get_forces(s)
+    f = ir1.sess.run(ir1.forces[s],feed_dict=ir1.feed_dict)
+
+
+print('\n---- reaxff_nn_torce ----\n')
+for s in ir1.bop:
+    print('\n evdw \n',ir1.evdw[s])
+    print('\n ehb \n',ir1.ehb[s])  
+    print(ir1.Evdw[s].shape)
 
 print('\n---- irff ----\n')
 images = Trajectory('md.traj')
@@ -30,25 +64,25 @@ forces = images[0].get_forces()
 for i,img in enumerate(images):
     ir_.calculate(atoms=img)
     ir2.calculate(atoms=img)
-    print('--     IR     --      RForce     --     IRNP     --' )
-    print('E      : ',ir_.E,ir.E[s][i].item(),ir2.E)
-    print('Eover  : ',ir_.Eover.item(),ir.eover[s][i].item(),ir2.Eover)
-    print('Eunder : ',ir_.Eunder.item(),ir.eunder[s][i].item(),ir2.Eunder)
-    print('Elone  : ',ir_.Elone.item(),ir.elone[s][i].item(),ir2.Elone)
-    print('Eang   : ',ir_.Eang.item(),ir.eang[s][i].item(),ir2.Eang)
-    print('Epen   : ',ir_.Epen.item(),ir.epen[s][i].item(),ir2.Epen)
-    print('Etor   : ',ir_.Etor.item(),ir.etor[s][i].item(),ir2.Etor)
-    print('Efcon  : ',ir_.Efcon.item(),ir.efcon[s][i].item(),ir2.Efcon)
-    print('Evdw   : ',ir_.Evdw.item(),ir.evdw[s][i].item(),ir2.Evdw)
-    print('Ehb    : ',ir_.Ehb.item(),ir.ehb[s][i].item(),ir2.Ehb)
+    print('--     RTFlow    --      RTorce     --     IRNP     --' )
+    print('E      : ',E[i],ir.E[s][i].item(),ir2.E)
+    print('Ebond  : ',ebond[i],ir.ebond[s][i].item(),ir2.Ebond)
+    print('Eover  : ',eover[i],ir.eover[s][i].item(),ir2.Eover)
+    print('Eunder : ',eunder[i],ir.eunder[s][i].item(),ir2.Eunder)
+    print('Elone  : ',elone[i],ir.elone[s][i].item(),ir2.Elone)
+    print('Eang   : ',eang[i],ir.eang[s][i].item(),ir2.Eang)
+    print('Epen   : ',epen[i],ir.epen[s][i].item(),ir2.Epen)
+    print('Etor   : ',etor[i],ir.etor[s][i].item(),ir2.Etor)
+    print('Efcon  : ',efcon[i],ir.efcon[s][i].item(),ir2.Efcon)
+    print('Evdw   : ',evdw[i],ir.evdw[s][i].item(),ir2.Evdw)
+    print('Ehb    : ',ehb[i],ir.ehb[s][i].item(),ir2.Ehb)
     # print('\n IR-dpi \n',ir2.Dpil)
  
- 
-# print('\n----  forces  ----\n')
-# ir_.calculate(atoms=images[0])
-# for i in range(ir_.natom):
-#     print(ir_.results['forces'][i],'----' ,ir.force[s][0][i].detach().numpy(),
-#              '----',forces[i])
+# print(ir.force)
+print('\n----  forces  ----\n')
+ir_.calculate(atoms=images[0])
+for i in range(ir_.natom):
+    print(f[0][i],'----' ,ir.force[s][0][i].detach().numpy(),'----',forces[i])
 
 # get_gulp_forces(images)
 # print('\n lammps: \n')
