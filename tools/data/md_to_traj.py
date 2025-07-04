300a7bb4c289011a2391ab4172590f44d3b81241
FengGo <gfeng.alan@foxmail.com>
2025-07-04 12:45:35 +0000

    md_to_traj
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/data/md_to_traj.py
+++ tools/data/md_to_traj.py
@@ -6,16 +6,16 @@ import argparse
 from irff.dft.siesta import siesta_traj
 from ase.io.trajectory import TrajectoryWriter
 
-def md_out_to_traj(traj):
+def md_out_to_traj(traj='md.traj',fdf='in.fdf',label='siesta',out='siesta.out'):
     ''' siesta MD out to traj
     '''
     traj =  TrajectoryWriter(traj,mode='w')
     # current_dir = getcwd()
-    images = siesta_traj(label='Carbonate',fdf='INPUT.fdf',out='log.out')
+    images = siesta_traj(label=label,fdf=fdf,out=out)
     for atoms in images:
         traj.write(atoms=atoms)
     traj.close()
      
 
 if __name__=='__main__':
-   md_out_to_traj('md.traj')
+   md_out_to_traj(traj='md.traj',fdf='in.fdf',label='siesta',out='siesta.out')
