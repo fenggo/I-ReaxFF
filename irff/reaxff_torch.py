f3d844fa349ce3c0a4c4e4bedf92578eaa17ab23
FengGo <gfeng.alan@foxmail.com>
2025-07-30 00:36:27 +0000

    set matrix
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- irff/reaxff_torch.py
+++ irff/reaxff_torch.py
@@ -1756,19 +1756,21 @@ class ReaxFF_nn(nn.Module):
          
       if ffield.endswith('.json'):
          for key in self.m:
-             k = key.split('_')[0]
-             if k[0]=='f' and (k[-1]=='w' or k[-1]=='b'):
-                for i,m in enumerate(self.m[key]):
-                    # if isinstance(M, np.ndarray):
-                    if self.device['diff'].type == 'cpu':
-                       self.m_[key][i] = m.detach().numpy().tolist()
-                    else:
-                       self.m_[key][i] = m.cpu().detach().numpy().tolist()
-             else:
-                if self.device['diff'].type == 'cpu':
-                   self.m_[key] = self.m[key].detach().numpy().tolist()  # covert ndarray to list
+             k_ = key.split('_')
+             k  = k_[0]
+             if len(k_)>1:
+                if k[0]=='f' and (k[-1]=='w' or k[-1]=='b'):
+                    for i,m in enumerate(self.m[key]):
+                        # if isinstance(M, np.ndarray):
+                        if self.device['diff'].type == 'cpu':
+                        self.m_[key][i] = m.detach().numpy().tolist()
+                        else:
+                        self.m_[key][i] = m.cpu().detach().numpy().tolist()
                 else:
-                   self.m_[key] = self.m[key].cpu().detach().numpy().tolist()
+                    if self.device['diff'].type == 'cpu':
+                    self.m_[key] = self.m[key].detach().numpy().tolist()  # covert ndarray to list
+                    else:
+                    self.m_[key] = self.m[key].cpu().detach().numpy().tolist()
          # print(' * save parameters to file ...')
          fj = open(ffield,'w')
          j = {'p':self.p_,'m':self.m_,
