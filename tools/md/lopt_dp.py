931b7b7cb60bde70d9e6e8d18242923047b79400
FengGo <gfeng.alan@foxmail.com>
2025-08-14 13:26:44 +0000

    lattice opt
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

---
+++ tools/md/lopt_dp.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+from ase import Atom, Atoms
+from ase.io import read
+# from ase.calculators.lammpslib import LAMMPSlib
+from ase.calculators.lammpsrun import LAMMPS
+from ase.constraints import StrainFilter
+from ase.optimize import BFGS
+from ase.io.trajectory import Trajectory
+
+
+os.environ['ASE_LAMMPSRUN_COMMAND'] = 'mpirun -n 4 lammps'
+
+
+pf    = 'csp2.pb'
+files = [pf]
+### super cell defination
+x = 8   
+y = 8
+z = 1
+
+G = read('POSCAR.unitcell')*(x,y,z)
+
+
+#lammps= LAMMPSlib(lmpcmds=cmds, log_file='graphene.log')
+lammps = LAMMPS(files=files)
+# lammps.set(pair_style='quip')
+# lammps.set(pair_coeff=['* * Carbon_GAP_20_potential/Carbon_GAP_20.xml \"\" 6'])
+lammps.set(pair_style='deepmd {:s}'.format(pf))
+lammps.set(pair_coeff=['* *'])
+lammps.set(tmp_dir='./')
+# lammps.set(keep_tmp_files=True)
+G.calc = lammps
+print("Energy ", G.get_potential_energy())
+
+
+sf  = StrainFilter(G)
+opt = BFGS(sf)
+
+traj = Trajectory('path.traj', 'w', G)
+opt.attach(traj)
+
+opt.run(0.0000001)
+
+traj = Trajectory('path.traj')
+atoms = traj[-1]
+traj.close()
+
+if x>1 or y>1 or z>1:   # get unit cell
+   ncell     = x*y*z
+   natoms    = int(len(atoms)/ncell)
+   species   = atoms.get_chemical_symbols()
+   positions = atoms.get_positions()
+   forces    = atoms.get_forces()
+   cell      = atoms.get_cell()
+   cell      = [cell[0]/x, cell[1]/y,cell[2]/z]
+   u         = np.linalg.inv(cell)
+   pos_      = np.dot(positions[0:natoms], u)
+   posf      = np.mod(pos_, 1.0)          # aplling simple pbc conditions
+   pos       = np.dot(posf, cell)
+   atoms     = Atoms(species[0:natoms],pos,#forces=forces[0:natoms],
+                     cell=cell,pbc=[True,True,True])
+
+atoms.write('POSCAR.unitcell')
