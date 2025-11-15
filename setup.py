013b54be30fc017713944de055db9396cc9aea7c
FengGo <gfeng.alan@foxmail.com>
2025-11-15 06:37:32 +0000

    update setup.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(name="irff",
       download_url='{}/archive/{}.tar.gz'.format(url, __version__),
       license="LGPL",
       packages= find_packages(),
-      package_data={'': ['*.gen','*.json','*.pyx']},
-      ext_modules=cythonize(['irff/neighbor.pyx','irff/getNeighbor.pyx'],annotate=True))
+      package_data={'': ['*.gen','*.json']})
+      # ext_modules=cythonize(['irff/neighbor.pyx','irff/getNeighbor.pyx'],annotate=True))
 
 
