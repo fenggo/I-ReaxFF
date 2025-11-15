133b46649993505201b5e56711b5a406c144aaef
FengGo <gfeng.alan@foxmail.com>
2025-11-15 06:16:59 +0000

    update setup.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- setup.py
+++ setup.py
@@ -9,26 +9,24 @@ install with commond
   "python setup.py build_ext --inplace"
   "python setup install --user"
 or with command
+*  version 1.7.3 
+  Add the dftb calculator.
 *  version 1.7.2 
   Correct the usage of batch size.
 *  version 1.7.1 
   Remove of dependence of pandas.
-
 *  version 1.7.0 
   Force learning with TensorFlow.
-
 *  version 1.6.2 
   Bus fixes.
-
 *  version 1.6.1 
   Add force learning and PyTorch backend.
-
 *  version 1.6 
   Add a penalty term for translation-invariant.
 '''
 
 
-__version__ = '1.7.2'
+__version__ = '1.7.3'
 install_requires = ['numpy','ase'] # ,'tensorflow','matplotlib','paramiko','argh','scikit-learn','cython']
 url = "https://gitee.com/fenggo/I-ReaxFF"
 
