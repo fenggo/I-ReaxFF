b49653ce73e50c9ab6e66c4b328f2b62b07c76f1
FengGo <gfeng.alan@foxmail.com>
2025-12-10 02:03:04 +0000

    update tools/uspex/uspex.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/uspex/uspex.py
+++ tools/uspex/uspex.py
@@ -8,7 +8,11 @@ import subprocess
 
 while True:
     subprocess.call('./run-octave USPEX.m > log',shell=True)
-    output = subprocess.check_output('cat log | tail -2',shell=True).decode('utf-8')
+    output = subprocess.check_output('cat log | tail -1',shell=True).decode('utf-8')
+    i = 1
+    while not output.strip():
+       i += 1
+       output = subprocess.check_output('cat log | tail -{:d}'.format(i),shell=True).decode('utf-8')
     
     if output.find('Relaxation is done')<0 and output.find('USPEX IS DONE')<0:
        subprocess.call('rm still_running',shell=True)
