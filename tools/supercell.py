6aa425962f9337f49f2f630f8cddf022f58367d6
FengGo <gfeng.alan@foxmail.com>
2026-03-30 13:20:10 +0000

    update tools/supercell.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- tools/supercell.py
+++ tools/supercell.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
-from irff.molecule import packmol
+import sys
+# import argh
+import argparse
 from ase.io import read,write
-from ase import build
+# from ase import build
+from irff.molecule import SuperCell
 
+help_ = 'run with commond: ./supercell.py --g=nm.gen --x=1 --y=1 --z=2 '
+parser = argparse.ArgumentParser(description=help_)
+parser.add_argument('--g',default='poscar.gen',type=str, help='training steps')
+parser.add_argument('--x',default=1,type=int, help='x supercell factor')
+parser.add_argument('--y',default=1,type=int, help='y supercell factor')
+parser.add_argument('--z',default=1,type=int, help='z supercell factor')
+args    = parser.parse_args(sys.argv[1:])
 
-A = read('POSCAR')
+A = read(args.g)
 # build.make_supercell(A,[2,2,2])
-write('poscar.gen',A*(2,2,2))
+# write('poscar.gen',A*(2,2,2))
 
-packmol(strucs=['poscar.gen'],
-        supercell=[3,3,2],
-        w=True)
+_,atoms = SuperCell(A,fac=1.0,supercell=None)
+write('POSCAR.supercell',atoms)
